acf437aba5a192df1324b4c18f01d54e4a734689
Nathan Jaremko <njaremko@gmail.com>
2018-07-02 10:09:43 -0400

    Remove dependency on ascii crate

--- src/utils/trytes_converter.rs
+++ src/utils/trytes_converter.rs
@@ -1,6 +1,37 @@
 use super::constants;
-use ascii::{AsciiChar, AsciiStr};
 use failure::Error;
+use std::collections::HashMap;
+
+lazy_static! {
+    static ref CHAR_TO_ASCII_MAP: HashMap<char, usize> = {
+        let mut res: HashMap<char, usize> = HashMap::new();
+        let mut ascii = 65;
+        for c in "ABCDEFGHIJKLMNOPQRSTUVWXYZ".chars() {
+            res.insert(c, ascii);
+            ascii += 1;
+        }
+        ascii = 97;
+        for c in "abcdefghijklmnopqrstuvwxyz".chars() {
+            res.insert(c, ascii);
+            ascii += 1;
+        }
+        ascii = 48;
+         for c in "0123456789".chars() {
+            res.insert(c, ascii);
+            ascii += 1;
+        }
+        res.insert(' ', 32);
+        res
+    };
+
+    static ref ASCII_TO_CHAR_MAP: HashMap<usize, char> = {
+        let mut res: HashMap<usize, char> = HashMap::new();
+        for (key, val) in CHAR_TO_ASCII_MAP.iter() {
+            res.insert(*val, *key);
+        }
+        res
+    };
+}
 
 #[derive(Debug, Fail)]
 enum TryteConverterError {
@@ -10,9 +41,14 @@ enum TryteConverterError {
 
 pub fn to_trytes(input: &str) -> Result<String, Error> {
     let mut trytes = String::new();
-    let tmp = AsciiStr::from_ascii(input)?;
-    for byte in tmp.chars() {
-        let mut ascii = byte.as_byte() as usize;
+    let mut tmp_ascii = Vec::new();
+    for c in input.chars() {
+        if let Some(ascii) = CHAR_TO_ASCII_MAP.get(&c) {
+            tmp_ascii.push(ascii);
+        }
+    }
+    for byte in tmp_ascii {
+        let mut ascii = *byte;
         if ascii > 255 {
             ascii = 32;
         }
@@ -25,9 +61,7 @@ pub fn to_trytes(input: &str) -> Result<String, Error> {
 }
 
 pub fn to_string(input_trytes: &str) -> Result<String, Error> {
-    if input_trytes.len() % 2 != 0 {
-        return Ok(String::new());
-    }
+    ensure!(input_trytes.len() % 2 == 0, "Invalid input tryte length.");
     let mut tmp = String::new();
     let chars: Vec<char> = input_trytes.chars().collect();
     for letters in chars.chunks(2) {
@@ -54,7 +88,9 @@ pub fn to_string(input_trytes: &str) -> Result<String, Error> {
             }
         };
         let decimal = first + second * 27;
-        tmp.push(AsciiChar::from(decimal as u8)?.as_char());
+        if let Some(t) = ASCII_TO_CHAR_MAP.get(&decimal) {
+            tmp.push(*t);
+        }
     }
     Ok(tmp)
 }
@@ -83,7 +119,8 @@ mod tests {
             .sample_iter(&Alphanumeric)
             .take(1000)
             .collect();
-        let back = to_string(&to_trytes(&s).unwrap()).unwrap();
+        let trytes = to_trytes(&s).unwrap();
+        let back = to_string(&trytes).unwrap();
         assert_eq!(s, back);
     }
 }
