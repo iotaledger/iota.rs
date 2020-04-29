531fb2bb42c0d3c4e6e878decfd8e0efe952d9f2
YuWeiWu <wusyong9104@gmail.com>
2020-04-29 12:57:37 +0800

    Propose iota-sdk as main crate

--- examples/get-node-info.rs
+++ examples/get-node-info.rs
@@ -6,11 +6,10 @@
 //! cargo run --example get-node-info
 //! ```
 use anyhow::Result;
-use iota_client;
 
 #[smol_potat::main]
 async fn main() -> Result<()> {
-    let iota = iota_client::Client::new("https://nodes.comnet.thetangle.org");
+    let iota = iota::Client::new("https://nodes.comnet.thetangle.org");
     let node_info = iota.get_node_info().await?;
     println!("{:#?}", node_info);
     Ok(())
