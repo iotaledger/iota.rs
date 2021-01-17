2f03ab8efe13f5ff9c087ac697e2c0f32af7a818
bingyanglin <bingyang.lin@iota.org>
2021-01-17 11:46:13 +0800

    Add full nodes API implementation

--- bindings/python/native/src/lib.rs
+++ bindings/python/native/src/lib.rs
@@ -36,7 +36,7 @@ struct Client {
 #[derive(Clone, DeriveFromPyObject, DeriveIntoPyObject)]
 pub struct MilestoneMetadata {
     /// Milestone index
-    pub milestone_index: u64,
+    pub index: u64,
     /// Milestone ID
     pub message_id: String,
     /// Timestamp
@@ -401,7 +401,16 @@ impl Client {
             amount: output_metadata.amount,
         }
     }
-    // fn get_address_balance(&self) {} Duplicated w/ get_address_balances
+    fn get_address_balance(&self, address: &str) -> u64 {
+        let rt = tokio::runtime::Runtime::new().unwrap();
+        rt.block_on(async {
+            self.client
+                .get_address()
+                .balance(&RustBech32Address::from(address))
+                .await
+                .unwrap()
+        })
+    }
     fn get_address_outputs(&self, address: &str) -> Vec<UTXOInput> {
         let rt = tokio::runtime::Runtime::new().unwrap();
         let outputs = rt.block_on(async {
@@ -420,8 +429,41 @@ impl Client {
             })
             .collect()
     }
-    fn find_outputs(&self, output_id: Option<Vec<UTXOInput>>, addresses: Option<Vec<String>>) {}
-    fn get_milestone(&self, index: u64) {}
+    fn find_outputs(&self, output_ids: Option<Vec<String>>, addresses: Option<Vec<String>>) -> Vec<OutputMetadata> {
+        let output_ids: Vec<RustUTXOInput> = output_ids
+            .unwrap_or(vec![])
+            .iter()
+            .map(|input| RustUTXOInput::from_str(input).unwrap_or_else(|_| panic!("invalid input: {}", input)))
+            .collect();
+        let addresses: Vec<RustBech32Address> = addresses
+            .unwrap_or(vec![])
+            .iter()
+            .map(|address| RustBech32Address::from(&address[..]))
+            .collect();
+        let rt = tokio::runtime::Runtime::new().unwrap();
+        let output_metadata_vec =
+            rt.block_on(async { self.client.find_outputs(&output_ids[..], &addresses[..]).await.unwrap() });
+        output_metadata_vec
+            .iter()
+            .map(|metadata| OutputMetadata {
+                message_id: metadata.message_id.clone(),
+                transaction_id: metadata.transaction_id.clone(),
+                output_index: metadata.output_index,
+                is_spent: metadata.is_spent,
+                address: metadata.address.to_bech32(),
+                amount: metadata.amount,
+            })
+            .collect()
+    }
+    fn get_milestone(&self, index: u64) -> MilestoneMetadata {
+        let rt = tokio::runtime::Runtime::new().unwrap();
+        let milestone_metadata = rt.block_on(async { self.client.get_milestone(index).await.unwrap() });
+        MilestoneMetadata {
+            index: milestone_metadata.index,
+            message_id: milestone_metadata.message_id,
+            timestamp: milestone_metadata.timestamp,
+        }
+    }
 }
 
 /// A Python module implemented in Rust.
