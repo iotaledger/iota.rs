5e78a541c9fcfa3352e0412a7cf0efb7965d3517
bingyanglin <bingyang.lin@iota.org>
2021-01-23 21:42:08 +0800

    Add license

--- bindings/python/native/src/client/mqtt.rs
+++ bindings/python/native/src/client/mqtt.rs
@@ -1,3 +1,6 @@
+// Copyright 2020 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
 use crate::client::Client;
 use pyo3::{conversion::ToPyObject, exceptions, prelude::*, types::PyTuple};
 use std::convert::TryInto;
