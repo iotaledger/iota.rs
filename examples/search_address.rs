ed5024f078168cdba724d57bd8a2c67ded1e715a
Thoralf-M <thoralf.mue@gmail.com>
2021-01-25 00:09:39 +0100

    replace env crate and update examples

--- examples/search_address.rs
+++ examples/search_address.rs
@@ -3,8 +3,9 @@
 
 //! cargo run --example search_address --release
 use iota::{api::search_address, Client, Seed};
-#[macro_use]
-extern crate dotenv_codegen;
+extern crate dotenv;
+use dotenv::dotenv;
+use std::env;
 
 /// In this example we try to find the index of an address from a seed.
 #[tokio::main]
@@ -15,7 +16,8 @@ async fn main() {
         .finish()
         .unwrap();
 
-    let seed = Seed::from_ed25519_bytes(&hex::decode(dotenv!("seed")).unwrap()).unwrap(); // Insert your seed
+    dotenv().ok();
+    let seed = Seed::from_ed25519_bytes(&hex::decode(env::var("seed").unwrap()).unwrap()).unwrap();
 
     let address = iota
         .find_addresses(&seed)
