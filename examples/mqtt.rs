8db2eca82c677cbd87d70d0eee49097607106d94
Thoralf-M <thoralf.mue@gmail.com>
2021-02-11 12:57:48 +0100

    fix tests

--- examples/mqtt.rs
+++ examples/mqtt.rs
@@ -6,7 +6,7 @@ use iota::{BrokerOptions, Client, Topic};
 use std::sync::{mpsc::channel, Arc, Mutex};
 
 // To run this example you need to add `features = ["mqtt"]` to the import of iota-core in the Cargo.toml
-// like this: iota-core = { path = "../iota-core", features = ["mqtt"] }
+// like this: iota-core = { path = "../iota-core", features = ["mqtt"] } and uncomment the mqtt example below
 #[tokio::main]
 async fn main() {
     let mut iota = Client::builder() // Crate a client instance builder
