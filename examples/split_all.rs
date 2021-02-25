95d098830770c80d19d9d99bcae4093635b768b8
Thoralf-M <thoralf.mue@gmail.com>
2021-02-25 17:13:04 +0100

    fix get_balance and address_index with initial_address_index, add example

--- examples/split_all.rs
+++ examples/split_all.rs
@@ -1,7 +1,7 @@
 // Copyright 2021 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-//! cargo run --example send_all --release
+//! cargo run --example split_all --release
 use iota::{client::Result, Client, MessageId, Seed};
 use std::time::Duration;
 use tokio::time::sleep;
@@ -9,8 +9,7 @@ extern crate dotenv;
 use dotenv::dotenv;
 use std::env;
 
-/// In this example, we get the balance of the first account of the seed and send everything
-// Todo: automatically detect amount of inputs and if > 127 create multiple transactions
+/// In this example, we get the balance of the first account of the seed and send everything splitted to the second seed
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -22,20 +21,34 @@ async fn main() -> Result<()> {
     // Insert your seed in the .env. Since the output amount cannot be zero. The seed must contain non-zero balance.
     println!("This example uses dotenv, which is not safe for use in production.");
     dotenv().ok();
-    let seed_1 = Seed::from_bytes(&hex::decode(env::var("NONSECURE_USE_OF_DEVELOPMENT_SEED_2").unwrap())?)?;
+    let seed_1 = Seed::from_bytes(&hex::decode(env::var("NONSECURE_USE_OF_DEVELOPMENT_SEED_1").unwrap())?)?;
+    let seed_2 = Seed::from_bytes(&hex::decode(env::var("NONSECURE_USE_OF_DEVELOPMENT_SEED_2").unwrap())?)?;
 
-    let total_balance = iota.get_balance(&seed_1).with_initial_address_index(0).finish().await?;
+    let total_balance = iota.get_balance(&seed_1).finish().await?;
+    let mut available = total_balance.clone();
     println!("total_balance {}", total_balance);
-    let message = iota
-        .message()
-        .with_seed(&seed_1)
-        .with_output(
-            &"atoi1qzt0nhsf38nh6rs4p6zs5knqp6psgha9wsv74uajqgjmwc75ugupx3y7x0r".into(),
-            total_balance,
-        )?
-        .with_initial_address_index(0)
+    let addresses_from_seed_2 = iota
+        .get_addresses(&seed_2)
+        .with_range(0..available as usize / 1_000_000)
         .finish()
         .await?;
+    let mut message_builder = iota.message().with_seed(&seed_1);
+    for i in 0..total_balance / 1_000_000 {
+        let mut amount = 1_000_000;
+        // Don't add more than we have or is allowed; 1 less here for remaining iotas
+        if available <= 0 || i > 125 {
+            break;
+        }
+        available -= amount;
+        // Add last amount so we don't create dust
+        if available < amount {
+            amount += available;
+            available = 0;
+        }
+        message_builder = message_builder.with_output(&addresses_from_seed_2[i as usize], amount)?;
+    }
+
+    let message = message_builder.finish().await?;
     println!(
         "Transaction sent: https://explorer.iota.org/chrysalis/message/{}",
         message.id().0
