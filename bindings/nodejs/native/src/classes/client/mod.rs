399c2520e5ba4ec63d71ce5fae691a7e7d45a393
melatron <antoni.dikov@gmail.com>
2021-03-04 16:06:24 +0200

    fmt fixed

--- bindings/nodejs/native/src/classes/client/mod.rs
+++ bindings/nodejs/native/src/classes/client/mod.rs
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use iota::{
-    message::prelude::{Address, Ed25519Address, MessageId, UTXOInput},
+    message::prelude::{Address, MessageId, UTXOInput},
     AddressOutputsOptions, OutputType, Seed,
 };
 use neon::prelude::*;
