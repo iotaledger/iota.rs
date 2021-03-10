6afcb6a0fd958e6d05996fe7ccf84b7a59ac47c8
bingyanglin <bingyang.lin@iota.org>
2021-03-10 15:54:18 +0800

    Add test vectors json

--- bindings/python/native/tests/test_api.py
+++ bindings/python/native/tests/test_api.py
@@ -2,19 +2,15 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import iota_client
+import json
 import os
 
-TEST_NODE_URL = "https://api.lb-0.testnet.chrysalis2.com"
+# Read the test vector
+tv = dict()
+with open('../../../fixtures/test_vectors.json') as json_file:
+    tv = json.load(json_file)
 
-# NOTE! Load the seed from your env path instead
-# NEVER assign the seed directly in your codes!
-# DO NOT USE THIS!!
-# SEED = "256a818b2aac458941f7274985a410e57fb750f3a3a67969ece5bd9ae7eef5b2"
-
-# USE THIS INSTEAD
-SEED = os.getenv('NONSECURE_USE_OF_DEVELOPMENT_SEED_1')
-EMPTY_ADDRESS = "atoi1qzt0nhsf38nh6rs4p6zs5knqp6psgha9wsv74uajqgjmwc75ugupx3y7x0r"
-client = iota_client.Client(node=TEST_NODE_URL)
+client = iota_client.Client(node=tv['NODE_URL'])
 
 def test_get_health():
     health = client.get_health()
@@ -30,7 +26,7 @@ def test_get_tips():
 
 def test_get_addresses():
     address_changed_list = client.get_addresses(
-        seed=SEED, account_index=0, input_range_begin=0, input_range_end=10, get_all=True)
+        seed=tv['NONSECURE_SEED'][0], account_index=0, input_range_begin=0, input_range_end=10, get_all=True)
 
     assert isinstance(address_changed_list, list)
     # Get the (address, changed ) for the first found address
@@ -39,35 +35,35 @@ def test_get_addresses():
     assert isinstance(balance, dict)
     
 def test_get_address_balance():
-    balance = client.get_address_balance(EMPTY_ADDRESS)
+    balance = client.get_address_balance(tv['ADDRESS'][0])
     assert isinstance(balance, dict)
 
 def test_get_address_outputs():
-    outputs = client.get_address_outputs(EMPTY_ADDRESS)
+    outputs = client.get_address_outputs(tv['ADDRESS'][0])
     assert isinstance(outputs, list)
 
 def test_indexation_with_int_list_data():
     message_id_indexation = client.message(
-        index="Hello", data=[84, 97, 110, 103, 108, 101])
+        index=tv['INDEXATION']['INDEX'][0], data=tv['INDEXATION']['DATA'][0])
     assert isinstance(message_id_indexation, dict)
     
 def test_indexation_with_data_str():
     message_id_indexation = client.message(
-        index="Hi", data_str="Tangle")
+        index=tv['INDEXATION']['INDEX'][1], data_str=tv['INDEXATION']['DATA_STRING'][0])
     assert isinstance(message_id_indexation, dict)
     
 def test_get_message_index():
-    message_id_indexation_queried = client.get_message_index("Hello")
+    message_id_indexation_queried = client.get_message_index(tv['INDEXATION']['INDEX'][0])
     assert isinstance(message_id_indexation_queried, list)
 
 def test_find_messages():
-    messages = client.find_messages(indexation_keys=["Hello"])
+    messages = client.find_messages(indexation_keys=tv['INDEXATION']['INDEX'])
     assert isinstance(messages, list)
 
 def test_get_unspent_address():
-    unspent_addresses = client.get_unspent_address(seed=SEED)
+    unspent_addresses = client.get_unspent_address(seed=tv['NONSECURE_SEED'][0])
     assert isinstance(unspent_addresses, tuple)
 
 def test_get_balance_in_seed():
-    balance = client.get_balance(seed=SEED)
+    balance = client.get_balance(seed=tv['NONSECURE_SEED'][1])
     assert isinstance(balance, int)
