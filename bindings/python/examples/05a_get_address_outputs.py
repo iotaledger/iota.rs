5328c3f3050c2adfbc445537df542f0817d14f8f
Hribek25 <petr@zizkovi.name>
2021-04-11 12:45:51 +0200

    Fixed encoding transaction index to bytes

--- bindings/python/examples/05a_get_address_outputs.py
+++ bindings/python/examples/05a_get_address_outputs.py
@@ -4,5 +4,5 @@ client = iota_client.Client()
 outputs = client.get_address_outputs("atoi1qp9427varyc05py79ajku89xarfgkj74tpel5egr9y7xu3wpfc4lkpx0l86")
 for output in outputs:
     print(f"Output index: {output['index']}; raw transaction id: {output['transaction_id']}")
-    encoded_hex = "".join(f"{i:0>2x}" for i in output["transaction_id"]) + f"{output['index']:0>4x}"
+    encoded_hex = "".join(f"{i:0>2x}" for i in output["transaction_id"] + list(int(output["index"]).to_bytes(2, 'little')))
     print(f"`output_id` encoded in hex: {encoded_hex}")
