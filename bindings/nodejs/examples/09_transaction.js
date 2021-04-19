4f8e57e0333e6a726d6fd294b16f7cac6c6038d4
Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
2021-04-19 13:05:20 +0200

    fix transaction example (#506)

--- bindings/nodejs/examples/09_transaction.js
+++ bindings/nodejs/examples/09_transaction.js
@@ -9,7 +9,7 @@ async function run() {
     // client will connect to testnet by default
     const client = new ClientBuilder().build();
 
-    const messageId = await client.messageSender()
+    const messageId = await client.send()
         .seed(IOTA_SEED_SECRET)
         .output('atoi1qqydc70mpjdvl8l2wyseaseqwzhmedzzxrn4l9g2c8wdcsmhldz0ulwjxpz', 1000000)
         .submit();
