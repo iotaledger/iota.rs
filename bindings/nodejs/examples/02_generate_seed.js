8be172cc78a4d0bdc5bacf125438d8980e62c8e4
Thoralf-M <thoralf.mue@gmail.com>
2021-04-20 13:39:25 +0200

    add new lines at end of files

--- bindings/nodejs/examples/02_generate_seed.js
+++ bindings/nodejs/examples/02_generate_seed.js
@@ -1,4 +1,4 @@
-function run(){
+function run() {
     const crypto = require('crypto');
     var seed = crypto.createHash('sha256').update(crypto.randomBytes(256)).digest('hex');
     console.log(seed);
