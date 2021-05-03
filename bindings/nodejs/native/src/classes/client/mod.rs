7b382a6b98bc5a0211e537e5e4a4ae8c9b8ff56f
Thoralf-M <thoralf.mue@gmail.com>
2021-05-03 09:32:47 +0200

    Handle optional value

--- bindings/nodejs/native/src/classes/client/mod.rs
+++ bindings/nodejs/native/src/classes/client/mod.rs
@@ -603,11 +603,13 @@ declare_types! {
 
         method hexToBech32(mut cx) {
             let hex = cx.argument::<JsString>(0)?.value();
-            let bech32_hrp: Option<String> = match cx.argument_opt(1) {
-                Some(arg) => {
-                    Some(arg.downcast::<JsString>().or_throw(&mut cx)?.value())
-                },
-                None => Default::default(),
+            let bech32_hrp: Option<String> = if cx.len() > 2 {
+                    match cx.argument_opt(1){
+                        Some(arg) => Some(arg.downcast::<JsString>().or_throw(&mut cx)?.value()),
+                        None => Default::default(),
+                    }
+                } else {
+                    Default::default()
             };
 
             let cb = cx.argument::<JsFunction>(cx.len()-1)?;
