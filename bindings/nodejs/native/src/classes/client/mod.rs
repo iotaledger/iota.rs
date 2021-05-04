692670baacaa2beed8420c2db47012af0a156c90
Thoralf-M <thoralf.mue@gmail.com>
2021-05-04 15:14:52 +0200

    add #![allow(clippy::unnecessary_wraps)]

--- bindings/nodejs/native/src/classes/client/mod.rs
+++ bindings/nodejs/native/src/classes/client/mod.rs
@@ -1,6 +1,7 @@
 // Copyright 2021 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+#![allow(clippy::unnecessary_wraps)]
 use iota_client::{
     bee_message::prelude::{Address, MessageId, TransactionId, UtxoInput},
     AddressOutputsOptions, Client, OutputType, Seed,
