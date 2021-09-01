1b07126e7c2220428063c0b94d4cdec2ab4e5898
Thoralf-M <thoralf.mue@gmail.com>
2021-09-01 11:45:36 +0200

    update get message_data examples

--- bindings/nodejs/examples/07_get_message_data.js
+++ bindings/nodejs/examples/07_get_message_data.js
@@ -4,11 +4,19 @@ async function run() {
     // client will connect to testnet by default
     const client = new ClientBuilder().build();
 
+    // get message data by message id (get a random message id with getTips)
     const tips = await client.getTips();
     const message_data = await client.getMessage().data(tips[0]);
     const message_metadata = await client.getMessage().metadata(tips[0]);
     console.log(message_metadata);
     console.log(message_data);
+
+    // get indexation data by index
+    const messages = await client.getMessage().index("IOTA.RS BINDING - NODE.JS")
+    for (message in messages) {
+        const message = await client.getMessage().data(messages[0])
+        console.log(Buffer.from(message.message.payload.data, 'hex').toString('utf8'));
+    }
 }
 
 run()
