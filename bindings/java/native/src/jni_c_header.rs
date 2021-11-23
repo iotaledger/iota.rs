43d871f6e39fb7dde38ba72417e1aacc54c2532f
Minyong Li <minyong.li@iota.org>
2021-11-23 11:32:57 +0000

    bindings/java: suppress UB warnings from bindgen

--- bindings/java/native/src/jni_c_header.rs
+++ bindings/java/native/src/jni_c_header.rs
@@ -1,12 +1,15 @@
 // Copyright 2020 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+// deref_nullptr: supressing UB warnings in bindgen generated code. See:
+// https://github.com/rust-lang/rust-bindgen/issues/1651
 #![allow(
     non_upper_case_globals,
     dead_code,
     non_camel_case_types,
     improper_ctypes,
     non_snake_case,
+    deref_nullptr,
     clippy::unreadable_literal,
     clippy::const_static_lifetime
 )]
