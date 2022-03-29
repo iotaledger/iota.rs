4e4cf5da01945b500ada2bf69b15eaecc6d5515a
Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
2022-03-29 17:37:06 +0200

    Update networkinfo with wasm feature every time (#879)
    * Update networkinfo with wasm feature every time
    
    * Update src/client.rs
    
    Co-authored-by: Junde Yhi <minyong.li@iota.org>
    
    * Remove obsolete code
    
    Co-authored-by: Junde Yhi <minyong.li@iota.org>

--- src/client.rs
+++ src/client.rs
@@ -272,7 +272,10 @@ impl Client {
     pub async fn get_network_info(&self) -> Result<NetworkInfo> {
         let not_synced = self.network_info.read().map_or(true, |info| info.network_id.is_none());
 
-        if not_synced {
+        // For WASM we don't have the node syncing process, which updates the network_info every 60 seconds, but the PoW
+        // difficulty or the byte cost could change via a milestone, so we request the nodeinfo every time, so we don't
+        // create invalid transactions/messages
+        if not_synced || cfg!(feature = "wasm") {
             let info = self.get_info().await?.nodeinfo;
             let network_id = hash_network(&info.protocol.network_name).ok();
             {
