e7f3441bb20aa1d8e345d1a5dfd407798e726f95
Brord van Wierst <brord@iota.org>
2022-04-06 18:46:36 +0200

    Fixed pom

--- bindings/java/examples/java-app/src/test/java/org/iota/client/example/LibraryTest.java
+++ bindings/java/examples/java-app/src/test/java/org/iota/client/example/LibraryTest.java
@@ -2,6 +2,7 @@ package org.iota.client.example;
 
 import java.util.Arrays;
 
+import static org.junit.Assert.*;	
 import org.junit.Test;
 
 import org.iota.client.*;
@@ -27,17 +28,19 @@ public class LibraryTest {
 
             SecretKey secret_key = SecretKey.generate();
             String seed = RustHex.encode("NONSECURE_USE_OF_DEVELOPMENT_SEED_1");
-            // 4e4f4e5345435552455f5553455f4f465f444556454c4f504d454e545f534545445f31
+            assertEquals(seed.toString(), "4e4f4e5345435552455f5553455f4f465f444556454c4f504d454e545f534545445f31");
             System.out.println("seed: " + seed);
 
             // Send to the first address here using faucet
             System.out.println("Addresses old: ");
             String[] addresses_old = GetAddressesBuilder.fromOld(seed).withClient(node).withRange(0, 1).finish();
+            assertEquals(addresses_old[0], "atoi1qzzj3wa2c0m0mpe6s2v004037sjhyk7zgr7hj3umwgnanr9xy6c92qyz3c8");
             System.out.println(Arrays.toString(addresses_old));
             
             // Receive the migration on address here
             System.out.println("Addresses new: ");
             String[] addresses = GetAddressesBuilder.from(seed).withClient(node).withRange(0, 1).finish();
+            assertEquals(addresses[0], "atoi1qp5dzudmpxxz7xxlzez8w5ttefeanhpf9rju48ds5y2ellp6aauuztf0dyd");
             System.out.println(Arrays.toString(addresses));
             
             // Try to migrate the first 50 public addresses
@@ -51,8 +54,6 @@ public class LibraryTest {
                     String address_to_migrate_to = addresses[0];
                     Message message = node.migrate(seed, accountIndex, addressIndex, internal_address, address_to_migrate_to);
                     System.out.println("Message: " + message);
-                } else {
-                    System.out.println("Did not need to migrate address index: "+ addressIndex);
                 }
             }
         } catch (ClientException e) {
