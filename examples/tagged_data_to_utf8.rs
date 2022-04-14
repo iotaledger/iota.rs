f5079d446d8910311bb8de97bc0f2a52d86754d4
Samuel Rufinatscha <samuel.rufinatscha@iota.org>
2022-04-14 11:03:10 +0200

    Introduce `tagged_data_to_utf8()`, `tag_to_utf8()` and `data_to_utf8()` (#911)
    * Introduce `tagged_data_to_utf8()`, `tag_to_utf8()` and `data_to_utf8()`
    
    * Update license header for tagged_data_to_utf8.rs

---
+++ examples/tagged_data_to_utf8.rs
@@ -0,0 +1,21 @@
+// Copyright 2022 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+//! cargo run --example tagged_data_to_utf8 --release
+
+use bee_message::payload::TaggedDataPayload;
+use iota_client::{Client, Result};
+
+/// In this example we will UTF-8 encode the tag and the data of an `TaggedDataPayload`.
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let tag = hex::decode("68656c6c6f").unwrap();
+    let data = hex::decode("776f726c64").unwrap();
+
+    let (tag_utf8, data_utf8) = Client::tagged_data_to_utf8(&TaggedDataPayload::new(tag, data).unwrap()).unwrap();
+
+    println!("tag: {}\ndata: {}", tag_utf8, data_utf8);
+
+    Ok(())
+}
