7a3b705db84c07fe03b18a5273f01dc77376b0a3
oddgrd <oddgrd@gmail.com>
2022-04-17 13:28:35 +0200

    Restructure to match wallet.rs node-bindings

--- bindings/nodejs/lib/index.js
+++ bindings/nodejs/lib/index.js
@@ -1,7 +1,7 @@
 // Copyright 2021-2022 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-const addon = require('../build/Release/index.node');
+const addon = require('../index.node');
 const mh = require('./messageHandler.js');
 const {Client} = require('./client.js');
 
