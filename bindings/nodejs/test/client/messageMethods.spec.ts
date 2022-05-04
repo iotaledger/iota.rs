fbdfed8a8b54953b755b6cd1868858671efb2627
oddgrd <oddgrd@gmail.com>
2022-05-04 16:46:10 +0200

    Replace more ad hoc examples with tests

---
+++ bindings/nodejs/test/client/messageMethods.spec.ts
@@ -0,0 +1,52 @@
+import { Client } from '../../lib';
+import '../customMatchers';
+import 'dotenv/config';
+
+const client = new Client({
+    nodes: [
+        {
+            // Insert your node URL here.
+            url: process.env.NODE_URL || 'http://localhost:14265',
+            disabled: false,
+        },
+    ],
+    localPow: true,
+});
+
+describe('Message methods', () => {
+    it('sends a message json', async () => {
+        const message = await client.generateMessage();
+
+        const jsonMessageId = await client.postMessageJson(message);
+
+        expect(jsonMessageId).toBeValidMessageId();
+    });
+
+    it('gets message children', async () => {
+        const message = await client.generateMessage();
+        const messageId = await client.messageId(message);
+
+        const messageChildren = await client.getMessageChildren(messageId);
+
+        expect(messageChildren).toBeDefined();
+    });
+
+    it('finds messages by message IDs', async () => {
+        const messageIds = await client.getTips();
+
+        const messages = await client.findMessages(messageIds);
+
+        expect(messages.length).toBe(messageIds.length);
+    });
+
+    // TODO: Error: 404 message not found. However, if calling getMessageData/metadata
+    // on the same ID, the message is found
+    it.skip('gets raw message', async () => {
+        const message = await client.generateMessage();
+        const messageId = await client.messageId(message);
+
+        const messageRaw = await client.getMessageRaw(messageId);
+
+        expect(messageRaw).toBeDefined();
+    });
+});
---
+++ bindings/nodejs/test/client/utxoMethods.spec.ts
@@ -0,0 +1,90 @@
+import { Client } from '../../lib';
+import '../customMatchers';
+import 'dotenv/config';
+
+const client = new Client({
+    nodes: [
+        {
+            // Insert your node URL here.
+            url: process.env.NODE_URL || 'http://localhost:14265',
+            disabled: false,
+        },
+    ],
+    localPow: true,
+});
+
+describe('UTXO methods', () => {
+    it('gets receipts', async () => {
+        const info = await client.getInfo();
+        // @ts-ignore: INodeInfo type is incorrect?
+        const milestoneIndex = info.nodeinfo.status.confirmedMilestone.index;
+
+        const receipts = await client.getReceipts();
+        expect(receipts).toBeDefined();
+
+        const receiptsMigratedAt = await client.getReceiptsMigratedAt(
+            milestoneIndex,
+        );
+        expect(receiptsMigratedAt).toBeDefined();
+    });
+
+    it('gets treasury', async () => {
+        const treasury = await client.getTreasury();
+
+        expect(treasury).toBeDefined();
+    });
+
+    it('gets aliases output IDs', async () => {
+        const aliasesOutputIds = await client.aliasesOutputIds([
+            {
+                stateController:
+                    'rms1qpllaj0pyveqfkwxmnngz2c488hfdtmfrj3wfkgxtk4gtyrax0jaxzt70zy',
+            },
+        ]);
+
+        expect(aliasesOutputIds).toBeDefined();
+    });
+
+    it('gets nfts output IDs', async () => {
+        const nftsOutputIds = await client.nftsOutputIds([
+            {
+                address:
+                    'rms1qpllaj0pyveqfkwxmnngz2c488hfdtmfrj3wfkgxtk4gtyrax0jaxzt70zy',
+            },
+        ]);
+
+        expect(nftsOutputIds).toBeDefined();
+    });
+
+    it('gets foundries output IDs', async () => {
+        const foundriesOutputIds = await client.foundriesOutputIds([
+            {
+                address:
+                    'rms1qpllaj0pyveqfkwxmnngz2c488hfdtmfrj3wfkgxtk4gtyrax0jaxzt70zy',
+            },
+        ]);
+
+        expect(foundriesOutputIds).toBeDefined();
+    });
+
+    // TODO: get valid IDs to test with
+    it('get alias/nft/foundry outputId rejects with 404 for invalid IDs', async () => {
+        await expect(
+            client.aliasOutputId(
+                '0x03119f37e7ad40608fc7ab15db49390abc233648c95e78141ff2e298f60d7a95',
+            ),
+        ).rejects.toMatch('404');
+
+        await expect(
+            client.nftOutputId(
+                '0x03119f37e7ad40608fc7ab15db49390abc233648c95e78141ff2e298f60d7a95',
+            ),
+        ).rejects.toMatch('404');
+
+        await expect(
+            client.foundryOutputId(
+                '0x03119f37e7ad40608fc7ab15db49390abc233648c95e78141ff2e298f60d7a9541ff2e60d7a9',
+            ),
+        ).rejects.toMatch('404');
+    });
+});
