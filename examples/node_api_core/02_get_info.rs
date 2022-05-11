dd394b46c86321b3c81024321da93354fb325ca4
Thibault Martinez <thibault@iota.org>
2022-05-11 10:27:19 +0200

    Add comments in examples

--- examples/node_api_core/02_get_info.rs
+++ examples/node_api_core/02_get_info.rs
@@ -7,17 +7,21 @@ use iota_client::{Client, Result};
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    // Takes the node URL from command line argument or use localhost as default.
     let node = std::env::args()
         .nth(1)
         .unwrap_or_else(|| "http://localhost:14265".to_string());
+    // Creates a client instance with that node.
     let client = Client::builder()
         .with_node(&node)?
         .with_node_sync_disabled()
         .finish()
         .await?;
 
+    // Sends the request.
     let info = client.get_info().await?;
 
+    // Prints the response.
     println!("{:?}", info);
 
     Ok(())
