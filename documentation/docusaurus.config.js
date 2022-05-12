61339a81d93f971f68ec14f55b98806bb5ca2d96
Dr-Electron <dr-electr0n@protonmail.com>
2022-05-12 17:34:16 +0200

    Prepare for new wiki build system (#875)
    * Prepare for new wiki build system
    
    * Update to yarn 3.2.0 and add wiki cli
    
    * Cleanup PR and update cli
    
    * Add test action
    
    * Update docusaurus.config.js
    
    * Add cli build
    
    * Update test-docs-build.yml
    
    * Update .yarnrc.yml

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -1,113 +1,18 @@
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const path = require('path');
 
-/** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'iota.rs',
-  tagline: 'Official IOTA Client Software',
-  url: 'https://client-lib.docs.iota.org/',
-  baseUrl: '/',
-  onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'throw',
-  favicon: '/img/logo/favicon.ico',
-  organizationName: 'iotaledger', // Usually your GitHub org/user name.
-  projectName: 'iota.rs', // Usually your repo name.
-  stylesheets: [
-    'https://fonts.googleapis.com/css?family=Material+Icons',
-  ],
-  themeConfig: {
-    colorMode: {
-          defaultMode: "dark",
-          },
-    navbar: {
-      title: 'iota.rs',
-      logo: {
-        alt: 'IOTA',
-        src: 'img/logo/Logo_Swirl_Dark.png',
-      },
-      items: [{
-          type: 'doc',
-          docId: 'welcome',
-          position: 'left',
-          label: 'Documentation',
-        },
-        //        {to: '/blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/iotaledger/iota.rs',
-          label: 'GitHub',
-          position: 'right',
-        },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      links: [{
-          title: 'Documentation',
-          items: [{
-              label: 'Welcome',
-              to: '/docs/welcome',
-            },
-            {
-              label: 'Overview',
-              to: '/docs/overview/',
-            },
-            {
-              label: 'Libraries',
-              to: '/docs/libraries/overview',
-            },
-            {
-              label: 'Specification',
-              to: '/docs/specs',
-            },
-            {
-              label: 'Contribute',
-              to: '/docs/contribute',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Discord',
-              href: 'https://discord.iota.org/',
-            },
-          ],
-        },
-        {
-          title: 'Contribute',
-          items: [
-            {
-              label: 'GitHub',
-              href: 'https://github.com/iotaledger/iota.rs',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} IOTA Foundation, Built with Docusaurus.`,
-    },
-    prism: {
-      additionalLanguages: ['rust'],
-      theme: lightCodeTheme,
-      darkTheme: darkCodeTheme,
-    },
-  },
-  presets: [
+  plugins: [
     [
-      '@docusaurus/preset-classic',
+      '@docusaurus/plugin-content-docs',
       {
-        docs: {
-          remarkPlugins: [require('remark-code-import'), require('remark-import-partial')],
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl: 'https://github.com/iotaledger/iota.rs/tree/dev/documentation/',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/iota.css'),
-        },
-      },
+        id: 'iota-rs',
+        path: path.resolve(__dirname, 'docs'),
+        routeBasePath: 'iota.rs',
+        sidebarPath: path.resolve(__dirname, 'sidebars.js'),
+        editUrl: 'https://github.com/iotaledger/iota.rs/edit/production/documentation',
+        remarkPlugins: [require('remark-code-import'), require('remark-import-partial')],
+      }
     ],
   ],
-  plugins: [
-  ],
+  staticDirectories: [path.resolve(__dirname, 'static')],
 };
