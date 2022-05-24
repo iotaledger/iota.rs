724935e162060602e536db6adeea4d8fa1cb18c2
shufps <shufps80@gmail.com>
2022-05-24 16:02:39 +0200

    getting ledger nano signer to compile; updated to blocks

--- src/secret/ledger_nano.rs
+++ src/secret/ledger_nano.rs
@@ -17,6 +17,8 @@ use tokio::sync::Mutex;
 use super::{types::InputSigningData, GenerateAddressMetadata, SecretManage, SecretManageExt};
 use crate::secret::{LedgerStatus, PreparedTransactionData, RemainderData};
 
+use packable::PackableExt;
+
 /// Hardened const for the bip path.
 ///
 /// See also: <https://wiki.trezor.io/Hardened_and_non-hardened_derivation>.
@@ -47,8 +49,8 @@ impl SecretManage for LedgerSecretManager {
     async fn generate_addresses(
         &self,
         // https://github.com/satoshilabs/slips/blob/master/slip-0044.md
-        // current ledger app only supports IOTA_COIN_TYPE
-        _coin_type: u32,
+        // current ledger app only supports IOTA_COIN_TYPE and SHIMMER_COIN_TYPE
+        coin_type: u32,
         account_index: u32,
         address_indexes: Range<u32>,
         internal: bool,
@@ -64,7 +66,7 @@ impl SecretManage for LedgerSecretManager {
             bip32_change: if internal { 1 } else { 0 } | HARDENED,
         };
         // get ledger
-        let ledger = iota_ledger::get_ledger(bip32_account, self.is_simulator)?;
+        let ledger = iota_ledger::get_ledger(coin_type, bip32_account, self.is_simulator)?;
 
         // if it's not for syncing, then it's a new receiving / remainder address
         // that needs shown to the user
@@ -101,154 +103,153 @@ impl SecretManageExt for LedgerSecretManager {
         // lock the mutex to prevent multiple simultaneous requests to a ledger
         let _lock = self.mutex.lock().await;
 
-        // Get coin type and account index from first input
-        let (_coin_type, account_index) = match &prepared_transaction.inputs_data.first() {
-            Some(input) => {
-                match &input.chain {
+        let mut input_bip32_indices: Vec<iota_ledger::LedgerBIP32Index> = Vec::new();
+        let mut coin_type: Option<u32> = None;
+        let mut account_index: Option<u32> = None;
+
+        let input_len = prepared_transaction.inputs_data.len();
+
+        for input in &prepared_transaction.inputs_data {
+            let bip32_indices: Vec<u32> = match &input.chain {
+                Some(chain) => {
+                    chain
+                        .segments()
+                        .iter()
+                        // XXX: "ser32(i)". RTFSC: [crypto::keys::slip10::Segment::from_u32()]
+                        .map(|seg| u32::from_be_bytes(seg.bs()))
+                        .collect()
+                }
+                None => return Err(crate::Error::NoInputs),
+            };
+            // coin_type and account_index should be the same in each output
+            assert!(
+                (coin_type.is_none() || coin_type == Some(bip32_indices[0]))
+                    && (account_index.is_none() || account_index == Some(bip32_indices[1]))
+            );
+            coin_type = Some(bip32_indices[0]);
+            account_index = Some(bip32_indices[1]);
+            input_bip32_indices.push(iota_ledger::LedgerBIP32Index {
+                bip32_change: bip32_indices[2] | HARDENED,
+                bip32_index: bip32_indices[3] | HARDENED,
+            });
+        }
+
+        assert!(coin_type.is_some() && account_index.is_some());
+
+        // unwrap values
+        let coin_type = coin_type.unwrap();
+        let bip32_account = account_index.unwrap() | HARDENED;
+
+        let ledger = iota_ledger::get_ledger(coin_type, bip32_account, self.is_simulator)?;
+
+        // figure out the remainder address and bip32 index (if there is one)
+        let (has_remainder, remainder_address, remainder_bip32): (
+            bool,
+            Option<&bee_block::address::Address>,
+            iota_ledger::LedgerBIP32Index,
+        ) = match &prepared_transaction.remainder {
+            Some(a) => {
+                let remainder_bip32_indices: Vec<u32> = match &a.chain {
                     Some(chain) => {
-                        let raw: Vec<u32> = chain
+                        chain
                             .segments()
                             .iter()
                             // XXX: "ser32(i)". RTFSC: [crypto::keys::slip10::Segment::from_u32()]
                             .map(|seg| u32::from_be_bytes(seg.bs()))
-                            .collect();
-                        (raw[0], raw[1])
+                            .collect()
                     }
+                    // todo error
                     None => return Err(crate::Error::NoInputs),
-                }
+                };
+                (
+                    true,
+                    Some(&a.address),
+                    iota_ledger::LedgerBIP32Index {
+                        bip32_index: remainder_bip32_indices[3] | HARDENED,
+                        bip32_change: remainder_bip32_indices[2] | HARDENED,
+                    },
+                )
             }
-            None => return Err(crate::Error::NoInputs),
+            None => (false, None, iota_ledger::LedgerBIP32Index::default()),
         };
 
-        let bip32_account = account_index | HARDENED;
-        let _ledger = iota_ledger::get_ledger(bip32_account, self.is_simulator)?;
-        todo!()
+        let mut remainder_index = 0u16;
+        if has_remainder {
+            match &prepared_transaction.essence {
+                bee_block::payload::transaction::TransactionEssence::Regular(essence) => {
+                    // find the index of the remainder in the essence
+                    // this has to be done because outputs in essences are sorted
+                    // lexically and therefore the remainder is not always the last output.
+                    // The index within the essence and the bip32 index will be validated
+                    // by the hardware wallet.
+                    // The outputs in the essence already are sorted
+                    // at this place, so we can rely on their order and don't have to sort it again.
+                    for output in essence.outputs().iter() {
+                        match output {
+                            bee_block::output::Output::Basic(s) => {
+                                // todo verify if that's the correct expected behaviour
+                                for block in s.unlock_conditions().iter() {
+                                    if let bee_block::output::UnlockCondition::Address(e) = block {
+                                        if *remainder_address.unwrap() == *e.address() {
+                                            break;
+                                        }
+                                    }
+                                }
+                            }
+                            _ => {
+                                log::debug!("[LEDGER] unsupported output");
+                                return Err(crate::Error::LedgerMiscError);
+                            }
+                        }
+                        remainder_index += 1;
+                    }
+
+                    // was index found?
+                    if remainder_index as usize == essence.outputs().len() {
+                        log::debug!("[LEDGER] remainder_index not found");
+                        return Err(crate::Error::LedgerMiscError);
+                    }
+                }
+            }
+        }
+
+        // pack essence into bytes
+        let essence_bytes = prepared_transaction.essence.pack_to_vec();
+
+        // prepare signing
+        log::debug!("[LEDGER] prepare signing");
+        log::debug!(
+            "[LEDGER] {:?} {:?} {} {} {:?}",
+            input_bip32_indices,
+            essence_bytes,
+            has_remainder,
+            remainder_index,
+            remainder_bip32
+        );
+        ledger.prepare_signing(
+            input_bip32_indices,
+            essence_bytes,
+            has_remainder,
+            remainder_index,
+            remainder_bip32,
+        )?;
+
+        // show essence to user
+        // if denied by user, it returns with `DeniedByUser` Error
+        log::debug!("[LEDGER] await user confirmation");
+        ledger.user_confirm()?;
+
+        // sign
+        let signature_bytes = ledger.sign(input_len as u16)?;
+        let mut readable = &mut &*signature_bytes;
+        // unpack signature to unlockblocks
+        let mut unlock_blocks = Vec::new();
+        for _ in 0..input_len {
+            let unlock_block = Unlock::unpack_verified(&mut readable).map_err(|_| crate::Error::PackableError)?;
+            unlock_blocks.push(unlock_block);
+        }
+        Ok(unlock_blocks)
     }
-
-    //     let input_len = inputs.len();
-
-    //     // on essence finalization, inputs are sorted lexically before they are packed into bytes.
-    //     // we need the correct order of the bip32 indices before we can call PrepareSigning, but
-    //     // because inputs of the essence don't have bip32 indices, we need to sort it on our own too.
-    //     let mut inputs_data: Vec<AddressIndexRecorder> = Vec::new();
-    //     for input_signing_data in inputs {
-    //         let input = Input::Utxo(UtxoInput::new(
-    //             TransactionId::from_str(&input_signing_data.output_response.transaction_id)?,
-    //             input_signing_data.output_response.output_index,
-    //         )?);
-    //         // todo validate
-    //         let address_index = u32::from_be_bytes(input_signing_data.chain.clone().unwrap().segments()[3].bs());
-    //         let address_internal = u32::from_be_bytes(input_signing_data.chain.clone().unwrap().segments()[4].bs());
-    //         inputs_data.push(AddressIndexRecorder {
-    //             input,
-    //             bip32: LedgerBIP32Index {
-    //                 bip32_index: address_index | HARDENED,
-    //                 bip32_change: address_internal | HARDENED,
-    //             },
-    //         });
-    //     }
-    //     // inputs_data.sort_by(|a, b| a.input.cmp(&b.input));
-
-    //     // now extract the bip32 indices in the right order
-    //     let mut input_bip32_indices: Vec<LedgerBIP32Index> = Vec::new();
-    //     for recorder in inputs_data {
-    //         input_bip32_indices.push(recorder.bip32);
-    //     }
-
-    //     // figure out the remainder address and bip32 index (if there is one)
-    //     let (has_remainder, remainder_address, remainder_bip32): (
-    //         bool,
-    //         Option<&bee_block::address::Address>,
-    //         LedgerBIP32Index,
-    //     ) = match meta.remainder_deposit_address {
-    //         Some(a) => (
-    //             true,
-    //             Some(&a.address),
-    //             LedgerBIP32Index {
-    //                 bip32_index: a.key_index | HARDENED,
-    //                 bip32_change: if a.internal { 1 } else { 0 } | HARDENED,
-    //             },
-    //         ),
-    //         None => (false, None, LedgerBIP32Index::default()),
-    //     };
-
-    //     let mut remainder_index = 0u16;
-    //     if has_remainder {
-    //         match essence {
-    //             bee_block::payload::transaction::TransactionEssence::Regular(essence) => {
-    //                 // find the index of the remainder in the essence
-    //                 // this has to be done because outputs in essences are sorted
-    //                 // lexically and therefore the remainder is not always the last output.
-    //                 // The index within the essence and the bip32 index will be validated
-    //                 // by the hardware wallet.
-    //                 // The outputs in the essence already are sorted (done by `essence_builder.finish`)
-    //                 // at this place, so we can rely on their order and don't have to sort it again.
-    //                 for output in essence.outputs().iter() {
-    //                     match output {
-    //                         bee_block::output::Output::Basic(s) => {
-    //                             // todo verify if that's the correct expected behaviour
-    //                             for block in s.unlock_conditions() {
-    //                                 if let bee_block::output::UnlockCondition::Address(e) = block {
-    //                                     if *remainder_address.unwrap() == *e.address() {
-    //                                         break;
-    //                                     }
-    //                                 }
-    //                             }
-    //                         }
-    //                         _ => {
-    //                             log::debug!("[LEDGER] unsupported output");
-    //                             return Err(crate::Error::LedgerMiscError);
-    //                         }
-    //                     }
-    //                     remainder_index += 1;
-    //                 }
-
-    //                 // was index found?
-    //                 if remainder_index as usize == essence.outputs().len() {
-    //                     log::debug!("[LEDGER] remainder_index not found");
-    //                     return Err(crate::Error::LedgerMiscError);
-    //                 }
-    //             }
-    //         }
-    //     }
-
-    //     // pack essence into bytes
-    //     let essence_bytes = essence.pack_to_vec();
-
-    //     // prepare signing
-    //     log::debug!("[LEDGER] prepare signing");
-    //     log::debug!(
-    //         "[LEDGER] {:?} {:?} {} {} {:?}",
-    //         input_bip32_indices,
-    //         essence_bytes,
-    //         has_remainder,
-    //         remainder_index,
-    //         remainder_bip32
-    //     );
-    //     ledger.prepare_signing(
-    //         input_bip32_indices,
-    //         essence_bytes,
-    //         has_remainder,
-    //         remainder_index,
-    //         remainder_bip32,
-    //     )?;
-
-    //     // show essence to user
-    //     // if denied by user, it returns with `DeniedByUser` Error
-    //     log::debug!("[LEDGER] await user confirmation");
-    //     ledger.user_confirm()?;
-
-    //     // sign
-    //     let signature_bytes = ledger.sign(input_len as u16)?;
-    //     let mut readable = &mut &*signature_bytes;
-    //     // unpack signature to unlockblocks
-    //     let mut unlocks = Vec::new();
-    //     for _ in 0..input_len {
-    //         let unlock = Unlock::unpack_verified(&mut readable).map_err(|_| crate::Error::PackableError)?;
-    //         unlocks.push(unlock);
-    //     }
-    //     Ok(unlocks)
-    // }
 }
 
 impl LedgerSecretManager {
@@ -279,6 +280,7 @@ impl LedgerSecretManager {
 
         log::info!("get_ledger");
         let (connected_, locked) = match iota_ledger::get_ledger(
+            0x107a, /* FIXME */
             crate::secret::ledger_nano::HARDENED,
             self.is_simulator,
         )
