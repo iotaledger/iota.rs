00fec5dcab15054df2c222dc884abbe98dc9ae6a
Dr-Electron <dr-electr0n@protonmail.com>
2022-05-26 09:16:06 +0200

    Add documentation version info

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -11,6 +11,12 @@ module.exports = {
         sidebarPath: path.resolve(__dirname, 'sidebars.js'),
         editUrl: 'https://github.com/iotaledger/iota.rs/edit/production/documentation',
         remarkPlugins: [require('remark-code-import'), require('remark-import-partial')],
+        versions: {
+          current: {
+            label: 'Production',
+            badge: true
+          },
+        },
       }
     ],
   ],
