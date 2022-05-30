8b6b510ed14f55886ac9e6e577b2b428081bdaca
oddgrd <oddgrd@gmail.com>
2022-05-30 21:40:36 +0200

    Update preparedTransactionData type

--- bindings/nodejs/types/preparedTransactionData.ts
+++ bindings/nodejs/types/preparedTransactionData.ts
@@ -1,7 +1,11 @@
 // Copyright 2021-2022 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-import type { IOutputResponse, ITransactionEssence } from '@iota/types';
+import type {
+    IOutputMetadataResponse,
+    ITransactionEssence,
+    OutputTypes,
+} from '@iota/types';
 
 /**
  * Helper struct for offline signing
@@ -14,14 +18,21 @@ export interface IPreparedTransactionData {
     /**
      * Required address information for signing
      */
-    inputSigningDataEntries: IInputSigningData[];
+    inputsData: IInputSigningData[];
 }
 
 /**
  * Data for transaction inputs for signing and ordering of unlock blocks
  */
 export interface IInputSigningData {
-    outputResponse: IOutputResponse;
+    /**
+     * The output
+     */
+    output: OutputTypes;
+    /**
+     * The output metadata
+     */
+    outputMetaData: IOutputMetadataResponse;
     /**
      * The chain derived from seed, only for ed25519 addresses
      */
