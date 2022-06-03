8449fa932c3bef484f84ca43e7501841efeb508c
Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
2022-06-03 13:31:16 +0200

    Add examples for remaining indexer endpoints (#1046)
    * Add examples for remaining indexer endpoints
    
    * Address review

--- examples/node_api_indexer/02_get_alias_outputs.rs
+++ examples/node_api_indexer/02_get_alias_outputs.rs
@@ -1,8 +1,11 @@
 // Copyright 2022 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-//! `cargo run --example node_api_indexer_get_basic_outputs --release -- [NODE URL]`.
+//! `cargo run --example node_api_indexer_get_alias_outputs --release -- [NODE URL]`.
 
+use std::str::FromStr;
+
+use bee_block::output::AliasId;
 use iota_client::{node_api::indexer::query_parameters::QueryParameter, Client, Result};
 
 #[tokio::main]
@@ -21,13 +24,11 @@ async fn main() -> Result<()> {
 
     let address = "rms1qpllaj0pyveqfkwxmnngz2c488hfdtmfrj3wfkgxtk4gtyrax0jaxzt70zy";
 
-    // Get output ids of basic outputs that can be controlled by this address without further unlock constraints.
+    // Get output ids of outputs that can be controlled by this address.
     let output_ids = client
-        .basic_output_ids(vec![
-            QueryParameter::Address(address.to_string()),
-            QueryParameter::HasExpirationCondition(false),
-            QueryParameter::HasTimelockCondition(false),
-            QueryParameter::HasStorageReturnCondition(false),
+        .alias_output_ids(vec![
+            QueryParameter::Governor(address.to_string()),
+            QueryParameter::StateController(address.to_string()),
         ])
         .await?;
 
@@ -36,6 +37,11 @@ async fn main() -> Result<()> {
     // Get the outputs by their id.
     let outputs_responses = client.get_outputs(output_ids).await?;
     println!("Outputs: {outputs_responses:?}",);
+
+    // Get an alias output by its AliasId.
+    let alias_id = AliasId::from_str("0xd1d1e67e30effbc22671284531a5609b82969b030750468470faf03bf0afcb98")?;
+    let output_id = client.alias_output_id(alias_id).await?;
+    println!("Alias output: {output_id}");
 
     Ok(())
 }
--- examples/node_api_indexer/03_get_foundry_outputs.rs
+++ examples/node_api_indexer/03_get_foundry_outputs.rs
@@ -1,8 +1,11 @@
 // Copyright 2022 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-//! `cargo run --example node_api_indexer_get_basic_outputs --release -- [NODE URL]`.
+//! `cargo run --example node_api_indexer_get_foundry_outputs --release -- [NODE URL]`.
 
+use std::str::FromStr;
+
+use bee_block::output::FoundryId;
 use iota_client::{node_api::indexer::query_parameters::QueryParameter, Client, Result};
 
 #[tokio::main]
@@ -19,16 +22,11 @@ async fn main() -> Result<()> {
         .finish()
         .await?;
 
-    let address = "rms1qpllaj0pyveqfkwxmnngz2c488hfdtmfrj3wfkgxtk4gtyrax0jaxzt70zy";
+    let alias_address = "rms1ppdr9w5wmyg7phcd7q9exv2kvnu5rnwafftsehjpfwd6zxn83938xw83dtr";
 
-    // Get output ids of basic outputs that can be controlled by this address without further unlock constraints.
+    // Get output ids of foundry outputs that can be controlled by this address.
     let output_ids = client
-        .basic_output_ids(vec![
-            QueryParameter::Address(address.to_string()),
-            QueryParameter::HasExpirationCondition(false),
-            QueryParameter::HasTimelockCondition(false),
-            QueryParameter::HasStorageReturnCondition(false),
-        ])
+        .foundry_output_ids(vec![QueryParameter::AliasAddress(alias_address.to_string())])
         .await?;
 
     println!("Address output_ids {:?}", output_ids);
@@ -36,6 +34,12 @@ async fn main() -> Result<()> {
     // Get the outputs by their id.
     let outputs_responses = client.get_outputs(output_ids).await?;
     println!("Outputs: {outputs_responses:?}",);
+
+    // Get an foundry output by its FoundryId.
+    let foundry_id =
+        FoundryId::from_str("0x085a32ba8ed911e0df0df00b93315664f941cddd4a570cde414b9ba11a678962730100000000")?;
+    let output_id = client.foundry_output_id(foundry_id).await?;
+    println!("Foundry output: {output_id}");
 
     Ok(())
 }
