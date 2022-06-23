23679067e6d528674b718a53acf0178ebd459f75
Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
2022-06-23 11:06:13 +0200

    Update examples/ledger_nano_transaction.rs

--- examples/ledger_nano_transaction.rs
+++ examples/ledger_nano_transaction.rs
@@ -1,4 +1,4 @@
-// Copyright 2021 IOTA Stiftung
+// Copyright 2022 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
 //! cargo run --example ledger_nano_transaction --features=ledger_nano --release
