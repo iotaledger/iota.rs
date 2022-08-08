1eff86c249c18426438215e8e8e83e1bd5950603
Thoralf Müller <thoralf.mue@gmail.com>
2022-08-08 21:24:11 +0200

    Add logger

--- bindings/python/native/examples/logger.py
+++ bindings/python/native/examples/logger.py
@@ -1,28 +1,20 @@
-from iota_client import IotaClient, LedgerNanoSecretManager
+from iota_client import IotaClient, init_logger
+import json
 
-# In this example we will get the ledger status and generate an address
-# To use the ledger nano simulator clone https://github.com/iotaledger/ledger-shimmer-app, run `git submodule init && git submodule update --recursive`,
-# then `./build.sh -m nanos|nanox|nanosplus -s` and use `True` in `LedgerNanoSecretManager(True)`.
+# Create the log configuration, the log will be outputted in 'iota.rs.log'
+log_config = {
+    'name': 'iota.rs.log',
+    'levelFilter': 'debug',
+    'targetExclusions': ["h2", "hyper", "rustls"]
+    }
+
+log_config_str = json.dumps(log_config)
+
+init_logger(log_config_str)
 
 # Create an IotaClient instance
 client = IotaClient({'nodes': ['https://api.testnet.shimmer.network']})
 
-is_simulator = True
-
-secret_manager = LedgerNanoSecretManager(is_simulator)
-
-# Get the Ledger Nano status.
-ledger_nano_status = client.get_ledger_status(is_simulator)
-
-print(f'Ledger Nano status: {ledger_nano_status}')
-
-# Generate public address with custom account index and range.
-address = client.generate_addresses(secret_manager, {
-    "accountIndex": 0,
-    "range": {
-        "start": 0,
-        "end": 1,
-    },
-})
-
-print(f'Address: {address[0]}')
+# Get the node info
+node_info = client.get_info()
+print(f'{node_info}')
