fc244948d43bab4a47b6e5ed76b0cd5cf23be1fc
Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
2022-08-19 12:33:11 +0200

    Update src/api/block_builder/input_selection/sender_issuer.rs
    Co-authored-by: Thibault Martinez <thibault.martinez.30@gmail.com>

--- src/api/block_builder/input_selection/sender_issuer.rs
+++ src/api/block_builder/input_selection/sender_issuer.rs
@@ -1,7 +1,7 @@
 // Copyright 2022 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-//! sender and issuer feature input selection
+//! sender and issuer features input selection
 
 use std::collections::HashSet;
 
