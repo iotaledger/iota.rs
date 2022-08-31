9f242b9f60c13545d342c01161cdccb8c4c0e2f1
Thibault Martinez <thibault.martinez.30@gmail.com>
2022-08-31 16:17:46 +0200

    Put a space between nodeinfo (#1222)

--- bindings/nodejs/types/nodeInfo.ts
+++ bindings/nodejs/types/nodeInfo.ts
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 import type { INodeInfo } from '@iota/types';
 
-/** NodeInfo wrapper which contains the nodeinfo and the url from the node (useful when multiple nodes are used) */
+/** NodeInfo wrapper which contains the node info and the url from the node (useful when multiple nodes are used) */
 export interface INodeInfoWrapper {
     /** The node info */
     nodeInfo: INodeInfo;
