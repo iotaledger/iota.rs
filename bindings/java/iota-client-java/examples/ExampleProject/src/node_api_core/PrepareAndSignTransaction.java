d24dbe283563030a31aef2a21802fa639f2e6a21
samuel_rufi <samuel.rufinatscha@iota.org>
2022-09-09 16:44:21 +0200

    Add PrepareAndSignTransaction example

---
+++ bindings/java/iota-client-java/examples/ExampleProject/src/node_api_core/PrepareAndSignTransaction.java
@@ -0,0 +1,30 @@
+package node_api_core;
+
+import org.iota.Client;
+import org.iota.types.Block;
+import org.iota.types.ClientConfig;
+import org.iota.types.ClientException;
+import org.iota.types.ids.BlockId;
+import org.iota.types.secret.*;
+
+import java.util.Map;
+
+public class PrepareAndSignTransaction {
+    public static void main(String[] args) throws ClientException {
+        // Build the client.
+        Client client = new Client(new ClientConfig().withNodes(new String[]{"https://api.testnet.shimmer.network"}));
+
+        // Build the secret manager.
+        SecretManager secretManager = new MnemonicSecretManager("YOUR_SECRET_MNEMONIC");
+
+        // Build the output for the transaction.
+        String receiverAddress = client.generateAddresses(secretManager, new GenerateAddressesOptions().withRange(new Range(0, 1)))[0];
+        BuildBlockOptions.ClientBlockBuilderOutputAddress output = new BuildBlockOptions.ClientBlockBuilderOutputAddress(receiverAddress, Integer.toString(1000000));
+
+        // Build block.
+        Map.Entry<BlockId, Block> b = client.buildAndPostBlock(secretManager, new BuildBlockOptions());
+
+        // Print the block ID.
+        System.out.println(b.getKey());
+    }
+}
