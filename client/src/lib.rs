6b0db158731603cb3b3398555c04a8485b77e5c8
Thibault Martinez <thibault@iota.org>
2022-10-06 17:48:58 +0200

    Fmt

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -56,8 +56,7 @@ pub mod utils;
 
 pub use bee_pow as pow;
 pub use crypto::{self, keys::slip10::Seed};
-pub use iota_types::api as api_types;
-pub use iota_types::block;
+pub use iota_types::{api as api_types, block};
 pub use packable;
 pub use url::Url;
 
