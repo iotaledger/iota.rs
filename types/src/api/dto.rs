fdde070032c7ed8e26e80209cad7d011549b6bf7
Thibault Martinez <thibault@iota.org>
2022-10-06 16:16:52 +0200

    Serde feature

--- types/src/api/dto.rs
+++ types/src/api/dto.rs
@@ -5,16 +5,19 @@ use crate::block::payload::milestone::option::dto::ReceiptMilestoneOptionDto;
 
 /// Describes a peer.
 #[derive(Clone, Debug, Eq, PartialEq)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(rename_all = "camelCase")
+)]
 pub struct PeerDto {
     pub id: String,
-    #[serde(rename = "multiAddresses")]
     pub multi_addresses: Vec<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
     pub alias: Option<String>,
     pub relation: RelationDto,
     pub connected: bool,
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
     pub gossip: Option<GossipDto>,
 }
 
@@ -28,77 +31,74 @@ pub struct GossipDto {
 
 /// Describes the relation with the peer.
 #[derive(Clone, Debug, Eq, PartialEq)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(rename_all = "camelCase")
+)]
 pub enum RelationDto {
-    #[serde(rename = "known")]
     Known,
-    #[serde(rename = "unknown")]
     Unknown,
-    #[serde(rename = "autopeered")]
     Autopeered,
 }
 
 /// Describes the heartbeat of a node.
 #[derive(Clone, Debug, Default, Eq, PartialEq)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(rename_all = "camelCase")
+)]
 pub struct HeartbeatDto {
-    #[serde(rename = "solidMilestoneIndex")]
     pub solid_milestone_index: u32,
-    #[serde(rename = "prunedMilestoneIndex")]
     pub pruned_milestone_index: u32,
-    #[serde(rename = "latestMilestoneIndex")]
     pub latest_milestone_index: u32,
-    #[serde(rename = "connectedPeers")]
     pub connected_peers: u8,
-    #[serde(rename = "syncedPeers")]
     pub synced_peers: u8,
 }
 
 /// Describes metrics of a gossip stream.
 #[derive(Clone, Debug, Default, Eq, PartialEq)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(rename_all = "camelCase")
+)]
 pub struct MetricsDto {
-    #[serde(rename = "newBlocks")]
     pub new_blocks: u64,
-    #[serde(rename = "receivedBlocks")]
     pub received_blocks: u64,
-    #[serde(rename = "knownBlocks")]
     pub known_blocks: u64,
-    #[serde(rename = "receivedBlockRequests")]
     pub received_block_requests: u64,
-    #[serde(rename = "receivedMilestoneRequests")]
     pub received_milestone_requests: u64,
-    #[serde(rename = "receivedHeartbeats")]
     pub received_heartbeats: u64,
-    #[serde(rename = "sentBlocks")]
     pub sent_blocks: u64,
-    #[serde(rename = "sentBlockRequests")]
     pub sent_block_requests: u64,
-    #[serde(rename = "sentMilestoneRequests")]
     pub sent_milestone_requests: u64,
-    #[serde(rename = "sentHeartbeats")]
     pub sent_heartbeats: u64,
-    #[serde(rename = "droppedPackets")]
     pub dropped_packets: u64,
 }
 
 /// Describes a receipt.
 #[derive(Clone, Debug, Eq, PartialEq)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(rename_all = "camelCase")
+)]
 pub struct ReceiptDto {
     pub receipt: ReceiptMilestoneOptionDto,
-    #[serde(rename = "milestoneIndex")]
     pub milestone_index: u32,
 }
 
 /// Describes the ledger inclusion state of a transaction.
 #[derive(Clone, Debug, Eq, PartialEq)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(rename_all = "camelCase")
+)]
 pub enum LedgerInclusionStateDto {
-    #[serde(rename = "conflicting")]
     Conflicting,
-    #[serde(rename = "included")]
     Included,
-    #[serde(rename = "noTransaction")]
     NoTransaction,
 }
--- types/src/api/response.rs
+++ types/src/api/response.rs
@@ -17,17 +17,18 @@ use crate::{
 /// Response of GET /api/core/v2/info.
 /// Returns general information about the node.
 #[derive(Clone, Debug, PartialEq)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(rename_all = "camelCase")
+)]
 pub struct InfoResponse {
     pub name: String,
     pub version: String,
     pub status: StatusResponse,
-    #[serde(rename = "supportedProtocolVersions")]
     pub supported_protocol_versions: Vec<u8>,
     pub protocol: ProtocolResponse,
-    #[serde(rename = "pendingProtocolParameters")]
     pub pending_protocol_parameters: Vec<PendingProtocolParameter>,
-    #[serde(rename = "baseToken")]
     pub base_token: BaseTokenResponse,
     pub metrics: MetricsResponse,
     pub features: Vec<String>,
@@ -36,59 +37,65 @@ pub struct InfoResponse {
 /// Returned in [`InfoResponse`].
 /// Status information about the node.
 #[derive(Clone, Debug, Eq, PartialEq)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(rename_all = "camelCase")
+)]
 pub struct StatusResponse {
-    #[serde(rename = "isHealthy")]
     pub is_healthy: bool,
-    #[serde(rename = "latestMilestone")]
     pub latest_milestone: LatestMilestoneResponse,
-    #[serde(rename = "confirmedMilestone")]
     pub confirmed_milestone: ConfirmedMilestoneResponse,
-    #[serde(rename = "pruningIndex")]
     pub pruning_index: u32,
 }
 
 /// Returned in [`StatusResponse`].
 /// Information about the latest milestone.
 #[derive(Clone, Debug, Eq, PartialEq)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(rename_all = "camelCase")
+)]
 pub struct LatestMilestoneResponse {
     pub index: u32,
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
     pub timestamp: Option<u32>,
-    #[serde(rename = "milestoneId", skip_serializing_if = "Option::is_none")]
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
     pub milestone_id: Option<String>,
 }
 
 /// Returned in [`StatusResponse`].
 /// Information about the confirmed milestone.
 #[derive(Clone, Debug, Eq, PartialEq)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(rename_all = "camelCase")
+)]
 pub struct ConfirmedMilestoneResponse {
     pub index: u32,
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
     pub timestamp: Option<u32>,
-    #[serde(rename = "milestoneId", skip_serializing_if = "Option::is_none")]
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
     pub milestone_id: Option<String>,
 }
 
 /// Returned in [`InfoResponse`].
 /// Protocol information about the node.
 #[derive(Clone, Debug, PartialEq, Eq)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(rename_all = "camelCase")
+)]
 pub struct ProtocolResponse {
     pub version: u8,
-    #[serde(rename = "networkName")]
     pub network_name: String,
-    #[serde(rename = "bech32Hrp")]
     pub bech32_hrp: String,
-    #[serde(rename = "minPowScore")]
     pub min_pow_score: u32,
-    #[serde(rename = "belowMaxDepth")]
     pub below_max_depth: u8,
-    #[serde(rename = "rentStructure")]
     pub rent_structure: RentStructureResponse,
-    #[serde(rename = "tokenSupply")]
     pub token_supply: String,
 }
 
@@ -114,13 +121,15 @@ impl TryFrom<ProtocolResponse> for ProtocolParameters {
 /// Returned in [`InfoResponse`].
 /// Pending protocol parameters.
 #[derive(Clone, Debug, Eq, PartialEq)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(rename_all = "camelCase")
+)]
 pub struct PendingProtocolParameter {
-    #[serde(rename = "type")]
+    #[cfg_attr(feature = "serde", serde(rename = "type"))]
     pub kind: u8,
-    #[serde(rename = "targetMilestoneIndex")]
     pub target_milestone_index: u32,
-    #[serde(rename = "protocolVersion")]
     pub protocol_version: u8,
     pub params: String,
 }
@@ -128,29 +137,32 @@ pub struct PendingProtocolParameter {
 /// Returned in [`InfoResponse`].
 /// Information about the base token.
 #[derive(Clone, Debug, Eq, PartialEq)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(rename_all = "camelCase")
+)]
 pub struct BaseTokenResponse {
     pub name: String,
-    #[serde(rename = "tickerSymbol")]
     pub ticker_symbol: String,
     pub unit: String,
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
     pub subunit: Option<String>,
     pub decimals: u8,
-    #[serde(rename = "useMetricPrefix")]
     pub use_metric_prefix: bool,
 }
 
 /// Returned in [`InfoResponse`].
 /// Rent information about the node.
 #[derive(Clone, Debug, Eq, PartialEq)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(rename_all = "camelCase")
+)]
 pub struct RentStructureResponse {
-    #[serde(rename = "vByteCost")]
     pub v_byte_cost: u32,
-    #[serde(rename = "vByteFactorKey")]
     pub v_byte_factor_key: u8,
-    #[serde(rename = "vByteFactorData")]
     pub v_byte_factor_data: u8,
 }
 
@@ -167,20 +179,25 @@ impl From<RentStructureResponse> for RentStructure {
 /// Returned in [`InfoResponse`].
 /// Metric information about the node.
 #[derive(Clone, Debug, PartialEq)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(rename_all = "camelCase")
+)]
 pub struct MetricsResponse {
-    #[serde(rename = "blocksPerSecond")]
     pub blocks_per_second: f64,
-    #[serde(rename = "referencedBlocksPerSecond")]
     pub referenced_blocks_per_second: f64,
-    #[serde(rename = "referencedRate")]
     pub referenced_rate: f64,
 }
 
 /// Response of GET /api/core/v2/tips.
 /// Returns non-lazy tips.
 #[derive(Clone, Debug, Eq, PartialEq)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(rename_all = "camelCase")
+)]
 pub struct TipsResponse {
     pub tips: Vec<String>,
 }
@@ -188,17 +205,24 @@ pub struct TipsResponse {
 /// Response of POST /api/core/v2/blocks.
 /// Returns the block identifier of the submitted block.
 #[derive(Clone, Debug, Eq, PartialEq)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(rename_all = "camelCase")
+)]
 pub struct SubmitBlockResponse {
-    #[serde(rename = "blockId")]
     pub block_id: String,
 }
 
 /// Response of GET /api/core/v2/blocks/{block_id}.
 /// Returns a specific block.
 #[derive(Clone, Debug, Eq, PartialEq)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-#[serde(untagged)]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(rename_all = "camelCase"),
+    serde(untagged)
+)]
 pub enum BlockResponse {
     Json(BlockDto),
     Raw(Vec<u8>),
@@ -207,33 +231,39 @@ pub enum BlockResponse {
 /// Response of GET /api/core/v2/blocks/{block_id}/metadata.
 /// Returns the metadata of a block.
 #[derive(Clone, Debug, Eq, PartialEq)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(rename_all = "camelCase")
+)]
 pub struct BlockMetadataResponse {
-    #[serde(rename = "blockId")]
     pub block_id: String,
     pub parents: Vec<String>,
-    #[serde(rename = "isSolid")]
     pub is_solid: bool,
-    #[serde(rename = "referencedByMilestoneIndex", skip_serializing_if = "Option::is_none")]
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
     pub referenced_by_milestone_index: Option<u32>,
-    #[serde(rename = "milestoneIndex", skip_serializing_if = "Option::is_none")]
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
     pub milestone_index: Option<u32>,
-    #[serde(rename = "ledgerInclusionState", skip_serializing_if = "Option::is_none")]
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
     pub ledger_inclusion_state: Option<LedgerInclusionStateDto>,
-    #[serde(rename = "conflictReason", skip_serializing_if = "Option::is_none")]
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
     pub conflict_reason: Option<u8>,
-    #[serde(rename = "whiteFlagIndex", skip_serializing_if = "Option::is_none")]
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
     pub white_flag_index: Option<u32>,
-    #[serde(rename = "shouldPromote", skip_serializing_if = "Option::is_none")]
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
     pub should_promote: Option<bool>,
-    #[serde(rename = "shouldReattach", skip_serializing_if = "Option::is_none")]
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
     pub should_reattach: Option<bool>,
 }
 
 /// Response of GET /api/core/v2/outputs/{output_id}.
 /// Returns an output and its metadata.
 #[derive(Clone, Debug, Eq, PartialEq)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(rename_all = "camelCase")
+)]
 pub struct OutputResponse {
     pub metadata: OutputMetadataResponse,
     pub output: OutputDto,
@@ -242,27 +272,24 @@ pub struct OutputResponse {
 /// Response of GET /api/core/v2/outputs/{output_id}/metadata.
 /// Returns an output metadata.
 #[derive(Clone, Debug, Eq, PartialEq)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(rename_all = "camelCase")
+)]
 pub struct OutputMetadataResponse {
-    #[serde(rename = "blockId")]
     pub block_id: String,
-    #[serde(rename = "transactionId")]
     pub transaction_id: String,
-    #[serde(rename = "outputIndex")]
     pub output_index: u16,
-    #[serde(rename = "isSpent")]
     pub is_spent: bool,
-    #[serde(rename = "milestoneIndexSpent", skip_serializing_if = "Option::is_none")]
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
     pub milestone_index_spent: Option<u32>,
-    #[serde(rename = "milestoneTimestampSpent", skip_serializing_if = "Option::is_none")]
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
     pub milestone_timestamp_spent: Option<u32>,
-    #[serde(rename = "transactionIdSpent", skip_serializing_if = "Option::is_none")]
+    #[cfg_attr(feature = "serde", serde(skip_serializing_if = "Option::is_none"))]
     pub transaction_id_spent: Option<String>,
-    #[serde(rename = "milestoneIndexBooked")]
     pub milestone_index_booked: u32,
-    #[serde(rename = "milestoneTimestampBooked")]
     pub milestone_timestamp_booked: u32,
-    #[serde(rename = "ledgerIndex", default)]
     pub ledger_index: u32,
 }
 
@@ -270,7 +297,11 @@ pub struct OutputMetadataResponse {
 /// * GET /api/core/v2/receipts/{milestone_index}, returns all stored receipts for the given milestone index.
 /// * GET /api/core/v2/receipts, returns all stored receipts, independent of a milestone index.
 #[derive(Clone, Debug, Eq, PartialEq)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(rename_all = "camelCase")
+)]
 pub struct ReceiptsResponse {
     pub receipts: Vec<ReceiptDto>,
 }
@@ -278,9 +309,12 @@ pub struct ReceiptsResponse {
 /// Response of GET /api/core/v2/treasury.
 /// Returns all information about the treasury.
 #[derive(Clone, Debug, Eq, PartialEq)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(rename_all = "camelCase")
+)]
 pub struct TreasuryResponse {
-    #[serde(rename = "milestoneId")]
     pub milestone_id: String,
     pub amount: String,
 }
@@ -288,8 +322,12 @@ pub struct TreasuryResponse {
 /// Response of GET /api/core/v2/milestone/{milestone_index}.
 /// Returns information about a milestone.
 #[derive(Clone, Debug, Eq, PartialEq)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-#[serde(untagged)]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(rename_all = "camelCase"),
+    serde(untagged)
+)]
 pub enum MilestoneResponse {
     Json(MilestonePayloadDto),
     Raw(Vec<u8>),
@@ -298,46 +336,67 @@ pub enum MilestoneResponse {
 /// Response of GET /api/core/v2/milestone/{milestone_index}/utxo-changes.
 /// Returns all UTXO changes that happened at a specific milestone.
 #[derive(Clone, Debug, Eq, PartialEq)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(rename_all = "camelCase")
+)]
 pub struct UtxoChangesResponse {
     pub index: u32,
-    #[serde(rename = "createdOutputs")]
     pub created_outputs: Vec<String>,
-    #[serde(rename = "consumedOutputs")]
     pub consumed_outputs: Vec<String>,
 }
 
 /// Response of GET /api/core/v2/peers.
 /// Returns information about all peers of the node.
 #[derive(Clone, Debug, Eq, PartialEq)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(rename_all = "camelCase")
+)]
 pub struct PeersResponse(pub Vec<PeerDto>);
 
 /// Response of POST /api/core/v2/peers.
 /// Returns information about the added peer.
 #[derive(Clone, Debug, Eq, PartialEq)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(rename_all = "camelCase")
+)]
 pub struct AddPeerResponse(pub PeerDto);
 
 /// Response of GET /api/core/v2/peer/{peer_id}.
 /// Returns information about a specific peer of the node.
 #[derive(Clone, Debug, Eq, PartialEq)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(rename_all = "camelCase")
+)]
 pub struct PeerResponse(pub PeerDto);
 
 /// Response of GET /api/plugins/debug/whiteflag.
 /// Returns the computed merkle tree hash for the given white flag traversal.
 #[derive(Clone, Debug, Eq, PartialEq)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(rename_all = "camelCase")
+)]
 pub struct WhiteFlagResponse {
-    #[serde(rename = "merkleTreeHash")]
     pub merkle_tree_hash: String,
 }
 
 /// Response of GET /api/routes.
 /// Returns the available API route groups of the node.
 #[derive(Clone, Debug, Eq, PartialEq)]
-#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Serialize, serde::Deserialize),
+    serde(rename_all = "camelCase")
+)]
 pub struct RoutesResponse {
     pub routes: Vec<String>,
 }
