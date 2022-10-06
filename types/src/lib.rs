db3744ddab74bcf5453ab7774bcfd0ea3115d59c
Thibault Martinez <thibault@iota.org>
2022-10-06 16:00:22 +0200

    Nits

--- types/src/lib.rs
+++ types/src/lib.rs
@@ -1,7 +1,7 @@
 // Copyright 2020-2022 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-//!
+//! Common types required by nodes and clients APIs like blocks, responses and DTOs.
 
 #![cfg_attr(not(feature = "std"), no_std)]
 #![cfg_attr(doc_cfg, feature(doc_cfg))]
