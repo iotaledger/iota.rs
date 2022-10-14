adf7a6014839b3f24f9ada613c8d82fa26e9f025
Thoralf Müller <thoralf.mue@gmail.com>
2022-10-14 19:51:07 +0200

    Show correct limit

--- client/src/api/block_builder/transaction.rs
+++ client/src/api/block_builder/transaction.rs
@@ -187,14 +187,14 @@ pub fn validate_regular_transaction_essence_length(
 
     // Max tx payload length - length for one signature unlock (there might be more unlocks, we check with them
     // later again, when we built the transaction payload)
-    if regular_transaction_essence_bytes.len()
-        > MAX_TX_LENGTH_FOR_BLOCK_WITH_8_PARENTS
-            - SINGLE_UNLOCK_LENGTH
-            - (reference_alias_nft_unlocks_amount * REFERENCE_ALIAS_NFT_UNLOCK_LENGTH)
-    {
+    let max_length = MAX_TX_LENGTH_FOR_BLOCK_WITH_8_PARENTS
+        - SINGLE_UNLOCK_LENGTH
+        - (reference_alias_nft_unlocks_amount * REFERENCE_ALIAS_NFT_UNLOCK_LENGTH);
+
+    if regular_transaction_essence_bytes.len() > max_length {
         return Err(Error::InvalidRegularTransactionEssenceLength {
             length: regular_transaction_essence_bytes.len(),
-            max_length: MAX_TX_LENGTH_FOR_BLOCK_WITH_8_PARENTS - SINGLE_UNLOCK_LENGTH,
+            max_length,
         });
     }
     Ok(())
