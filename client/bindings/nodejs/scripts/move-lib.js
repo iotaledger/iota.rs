73c67b14d0dff020ee92c1b9a5e9b23bd61c6eea
Thoralf Müller <thoralf.mue@gmail.com>
2022-10-25 09:44:15 +0200

    Fix prebuild scripts

--- client/bindings/nodejs/scripts/move-lib.js
+++ client/bindings/nodejs/scripts/move-lib.js
@@ -1,9 +1,15 @@
 const { existsSync, mkdirSync, renameSync } = require('fs');
 const { resolve } = require('path');
 
-const path = resolve(__dirname, '../build/Release');
+// Prebuild requires that the binary be in `build/Release` as though it was built with node-gyp
 
-if (!existsSync(path)) {
-    mkdirSync(path, { recursive: true });
-}
-renameSync(resolve(__dirname, '../native/index.node'), resolve(path, 'index.node'));
+const moveArtifact = () => {
+    const path = resolve(__dirname, '../build/Release');
+
+    if (!existsSync(path)) {
+        mkdirSync(path, { recursive: true });
+    }
+    renameSync(resolve(__dirname, '../index.node'), resolve(path, 'index.node'));
+};
+
+module.exports = moveArtifact;
