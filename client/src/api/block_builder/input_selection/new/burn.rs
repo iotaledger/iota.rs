e42a0884146249561527615d6a0547dac961c0ec
Thibault Martinez <thibault@iota.org>
2022-11-07 09:54:15 +0100

    Add InputSelection and Burn

---
+++ client/src/api/block_builder/input_selection/new/burn.rs
@@ -0,0 +1,19 @@
+// Copyright 2022 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+use std::collections::{HashMap, HashSet};
+
+use primitive_types::U256;
+
+use crate::block::output::{AliasId, FoundryId, NftId, TokenId};
+
+pub(crate) struct Burn {
+    /// Aliases to burn.
+    pub(crate) aliases: HashSet<AliasId>,
+    /// NFTs to burn.
+    pub(crate) nfts: HashSet<NftId>,
+    /// Foundries to burn.
+    pub(crate) foundries: HashSet<FoundryId>,
+    /// Native tokens with the amount to burn.
+    pub(crate) native_tokens: HashMap<TokenId, U256>,
+}
--- client/src/api/block_builder/input_selection/new/mod.rs
+++ client/src/api/block_builder/input_selection/new/mod.rs
@@ -1,4 +1,27 @@
 // Copyright 2022 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+mod burn;
 mod requirement;
+
+use burn::Burn;
+
+use crate::block::{
+    address::Address,
+    output::{Output, OutputId},
+    protocol::ProtocolParameters,
+};
+
+struct InputSelection {
+    outputs: Vec<Output>,
+    available_inputs: Vec<Output>,
+    protocol_parameters: ProtocolParameters,
+    time: u32,
+    required_inputs: Vec<OutputId>,
+    forbidden_inputs: Vec<OutputId>,
+    remainder_address: Option<Address>,
+    burn: Option<Burn>,
+    // TODO: decide if we want to add the addresses here to check if we can unlock an output or not:
+    // alias output can have two different addresses and expiration unlock condition can change the unlock address
+    // sender_addresses: Vec<Address>,
+}
