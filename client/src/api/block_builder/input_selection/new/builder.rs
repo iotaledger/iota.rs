945e30beccf04db784bb0081cb16c5f63955013c
Thibault Martinez <thibault@iota.org>
2022-11-07 10:46:44 +0100

    Add InputSelectionBuilder

---
+++ client/src/api/block_builder/input_selection/new/builder.rs
@@ -0,0 +1,89 @@
+// Copyright 2022 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+use super::{burn::Burn, InputSelection};
+use crate::{
+    block::{
+        address::Address,
+        output::{Output, OutputId},
+        protocol::ProtocolParameters,
+    },
+    secret::types::InputSigningData,
+};
+
+pub struct InputSelectionBuilder {
+    outputs: Vec<Output>,
+    // TODO impl Iter ?
+    available_inputs: Vec<InputSigningData>,
+    protocol_parameters: ProtocolParameters,
+    timestamp: Option<u32>,
+    required_inputs: Vec<OutputId>,
+    forbidden_inputs: Vec<OutputId>,
+    remainder_address: Option<Address>,
+    burn: Option<Burn>,
+    // TODO: decide if we want to add the addresses here to check if we can unlock an output or not:
+    // alias output can have two different addresses and expiration unlock condition can change the unlock address
+    // sender_addresses: Vec<Address>,
+}
+
+impl InputSelectionBuilder {
+    pub fn new(
+        outputs: Vec<Output>,
+        available_inputs: Vec<InputSigningData>,
+        protocol_parameters: ProtocolParameters,
+    ) -> Self {
+        Self {
+            outputs,
+            available_inputs,
+            protocol_parameters,
+            timestamp: None,
+            required_inputs: Vec::new(),
+            forbidden_inputs: Vec::new(),
+            remainder_address: None,
+            burn: None,
+        }
+    }
+
+    pub fn time(mut self, time: u32) -> Self {
+        self.timestamp.replace(time);
+        self
+    }
+
+    pub fn required_inputs(mut self, inputs: Vec<OutputId>) -> Self {
+        self.required_inputs = inputs;
+        self
+    }
+
+    pub fn forbidden_inputs(mut self, inputs: Vec<OutputId>) -> Self {
+        self.forbidden_inputs = inputs;
+        self
+    }
+
+    pub fn remainder_address(mut self, address: Address) -> Self {
+        self.remainder_address.replace(address);
+        self
+    }
+
+    pub fn burn(mut self, burn: Burn) -> Self {
+        self.burn.replace(burn);
+        self
+    }
+
+    pub fn build(self) -> InputSelection {
+        InputSelection {
+            outputs: self.outputs,
+            available_inputs: self.available_inputs,
+            protocol_parameters: self.protocol_parameters,
+            timestamp: self.timestamp.unwrap_or_else(|| {
+                instant::SystemTime::now()
+                    .duration_since(instant::SystemTime::UNIX_EPOCH)
+                    .expect("time went backwards")
+                    .as_secs() as u32
+            }),
+            required_inputs: self.required_inputs,
+            forbidden_inputs: self.forbidden_inputs,
+            remainder_address: self.remainder_address,
+            burn: self.burn,
+        }
+    }
+}
--- client/src/api/block_builder/input_selection/new/mod.rs
+++ client/src/api/block_builder/input_selection/new/mod.rs
@@ -1,23 +1,27 @@
 // Copyright 2022 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+mod builder;
 mod burn;
 mod requirement;
 
 use burn::Burn;
 
-use crate::block::{
-    address::Address,
-    output::{Output, OutputId},
-    protocol::ProtocolParameters,
+use crate::{
+    block::{
+        address::{Address, AliasAddress, NftAddress},
+        output::{Output, OutputId},
+        protocol::ProtocolParameters,
+    },
+    secret::types::InputSigningData,
 };
 
 pub struct InputSelection {
     outputs: Vec<Output>,
     // TODO impl Iter ?
-    available_inputs: Vec<Output>,
+    available_inputs: Vec<InputSigningData>,
     protocol_parameters: ProtocolParameters,
-    timestamp: Option<u32>,
+    timestamp: u32,
     required_inputs: Vec<OutputId>,
     forbidden_inputs: Vec<OutputId>,
     remainder_address: Option<Address>,
@@ -28,41 +32,24 @@ pub struct InputSelection {
 }
 
 impl InputSelection {
-    pub fn new(outputs: Vec<Output>, available_inputs: Vec<Output>, protocol_parameters: ProtocolParameters) -> Self {
-        Self {
-            outputs,
-            available_inputs,
-            protocol_parameters,
-            timestamp: None,
-            required_inputs: Vec::new(),
-            forbidden_inputs: Vec::new(),
-            remainder_address: None,
-            burn: None,
-        }
-    }
+    pub fn filter(mut self, addresses: &[Address]) -> Self {
+        let addresses = addresses
+            .iter()
+            // TODO meh
+            .copied()
+            .chain(self.available_inputs.iter().filter_map(|input| match &input.output {
+                // TODO unwrap ?
+                Output::Alias(output) => Some(Address::Alias(AliasAddress::from(
+                    output.alias_id().or_from_output_id(input.output_id().unwrap()),
+                ))),
+                Output::Nft(output) => Some(Address::Nft(NftAddress::from(
+                    output.nft_id().or_from_output_id(input.output_id().unwrap()),
+                ))),
+                _ => None,
+            }));
 
-    pub fn time(mut self, time: u32) -> Self {
-        self.timestamp.replace(time);
-        self
-    }
+        // self.available_inputs.retain(|input| input.can_be_unlocked_now(input, addresses, ???, self.time))
 
-    pub fn required_inputs(mut self, inputs: Vec<OutputId>) -> Self {
-        self.required_inputs = inputs;
-        self
-    }
-
-    pub fn forbidden_inputs(mut self, inputs: Vec<OutputId>) -> Self {
-        self.forbidden_inputs = inputs;
-        self
-    }
-
-    pub fn remainder_address(mut self, address: Address) -> Self {
-        self.remainder_address.replace(address);
-        self
-    }
-
-    pub fn burn(mut self, burn: Burn) -> Self {
-        self.burn.replace(burn);
         self
     }
 }
