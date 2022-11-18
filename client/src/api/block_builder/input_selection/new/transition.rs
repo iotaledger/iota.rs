632671b2b0d88f4a5819fc2c872657008831ef6c
Thibault Martinez <thibault@iota.org>
2022-11-18 11:12:43 +0100

    Fix transition burn checks

--- client/src/api/block_builder/input_selection/new/transition.rs
+++ client/src/api/block_builder/input_selection/new/transition.rs
@@ -2,39 +2,46 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use super::Burn;
-use crate::{block::output::Output, secret::types::InputSigningData};
+use crate::{
+    block::output::{AliasOutput, FoundryOutput, NftOutput, Output, OutputId},
+    secret::types::InputSigningData,
+};
 
-pub(crate) fn transition_input(input: &InputSigningData, outputs: &[Output], burn: Option<&Burn>) -> Option<Output> {
-    // match input {
-    //     Output::Alias(alias_output) => {
-    //         if burn.aliases.contains(alias_id) {
-    //             return None;
-    //         }
-
-    //         Some(
-    //                     // TODO create output from input
-    //                 )
-    //     }
-    //     Output::Nft(nft_input) => {
-    //         if burn.nfts.contains(nft_id) {
-    //             return None;
-    //         }
-
-    //         Some(
-    //                     // TODO create output from input
-    //                 )
-    //     }
-    //     Output::Foundry(foundry_output) => {
-    //         if burn.foundries.contains(foundry_id) {
-    //             return None;
-    //         }
-
-    //         Some(
-    //                     // TODO create output from input
-    //                 )
-    //     }
-    //     _ => None,
-    // }
+fn transition_alias_input(input: &AliasOutput, output_id: &OutputId, burn: Option<&Burn>) -> Option<Output> {
+    if burn
+        .map(|burn| burn.aliases.contains(&input.alias_id_non_null(output_id)))
+        .unwrap_or(false)
+    {
+        return None;
+    }
 
     None
+}
+
+fn transition_nft_input(input: &NftOutput, output_id: &OutputId, burn: Option<&Burn>) -> Option<Output> {
+    if burn
+        .map(|burn| burn.nfts.contains(&input.nft_id_non_null(output_id)))
+        .unwrap_or(false)
+    {
+        return None;
+    }
+
+    None
+}
+
+fn transition_foundry_input(input: &FoundryOutput, burn: Option<&Burn>) -> Option<Output> {
+    if burn.map(|burn| burn.foundries.contains(&input.id())).unwrap_or(false) {
+        return None;
+    }
+
+    None
+}
+
+pub(crate) fn transition_input(input: &InputSigningData, outputs: &[Output], burn: Option<&Burn>) -> Option<Output> {
+    match &input.output {
+        Output::Alias(alias_input) => transition_alias_input(alias_input, input.output_id(), burn),
+        Output::Nft(nft_input) => transition_nft_input(nft_input, input.output_id(), burn),
+        Output::Foundry(foundry_input) => transition_foundry_input(foundry_input, burn),
+        _ => None,
+    }
 }
