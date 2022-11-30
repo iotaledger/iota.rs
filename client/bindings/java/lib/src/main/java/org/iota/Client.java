8c16f6ba029cc904bb612eaf8d8878936bfad986
Samuel Rufinatscha <samuel.rufinatscha@gmail.com>
2022-11-30 19:40:41 +0100

    Update client/bindings/java/lib/src/main/java/org/iota/Client.java
    Co-authored-by: Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>

--- client/bindings/java/lib/src/main/java/org/iota/Client.java
+++ client/bindings/java/lib/src/main/java/org/iota/Client.java
@@ -534,11 +534,11 @@ public class Client extends NativeApi {
      * Converts an alias id to a bech32 address.
      *
      * @param aliasId The alias id to be converted.
-     * @param bech32 The bech32 to use.
+     * @param bech32Hrp The bech 32 human readable part to use.
      * @return The bech32 address.
      * @throws ClientException on error.
      */
-    public String aliasIdToBech32(AliasId aliasId, String bech32) throws ClientException {
+    public String aliasIdToBech32(AliasId aliasId, String bech32Hrp) throws ClientException {
         return utilsApi.aliasIdToBech32(aliasId, bech32);
     }
 
