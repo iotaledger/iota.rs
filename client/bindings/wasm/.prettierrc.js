dd3ac9e41203a71aab265df82b7b5fcebd1f7692
Oliver E. Anderson <oliver.anderson@iota.org>
2022-12-01 16:54:34 +0100

    Generate wasm bindings (#1416)
    * Added WASM bindings from previous branch
    
    * Add wasm-bindgen to requirements
    
    * Add `publish = false` to other bindings (good idea!)
    
    * Cargo toml nits
    
    * Remove 2021 from copyrights
    
    * lib.rs nits
    
    * Update client/bindings/wasm/.eslintrc.js
    
    Co-authored-by: Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
    
    * Changelog
    
    * Change package name and version
    
    * Rename lib-wasm to lib
    
    * message_handler nits
    
    Co-authored-by: Thibault Martinez <thibault@iota.org>
    Co-authored-by: Thibault Martinez <thibault.martinez.30@gmail.com>
    Co-authored-by: Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>

---
+++ client/bindings/wasm/.prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    semi: true,
+    singleQuote: true,
+    tabWidth: 4,
+    useTabs: false,
+    trailingComma: 'all',
+    bracketSpacing: true,
+    arrowParens: 'always',
+};
---
+++ client/bindings/wasm/build_scripts/node.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const fs = require('fs');
+const { lintAll } = require('./lints');
+const generatePackage = require('./utils/generatePackage');
+
+const rustPackageName = "client_wasm";
+
+const RELEASE_FOLDER = path.join(__dirname, '../node/wasm/');
+const entryFilePathNode = path.join(RELEASE_FOLDER, rustPackageName + '.js');
+const entryFileNode = fs.readFileSync(entryFilePathNode).toString();
+
+// copy TypeScript files into temporary directory.
+// replace bindings.ts with local version.
+
+lintAll(entryFileNode);
+
+// Add node-fetch polyfill (https://github.com/seanmonstar/reqwest/issues/910).
+let changedFileNode = entryFileNode.replace(
+    "let imports = {};",
+    `if (!globalThis.fetch) {
+    const fetch = require('node-fetch')
+    globalThis.Headers = fetch.Headers
+    globalThis.Request = fetch.Request
+    globalThis.Response = fetch.Response
+    globalThis.fetch = fetch
+}
+let imports = {};`);
+
+fs.writeFileSync(
+    entryFilePathNode,
+    changedFileNode
+);
+
+const newPackage = generatePackage({
+    main: 'lib/index.js',
+    types: 'lib/index.d.ts',
+});
+
+fs.writeFileSync(path.join(RELEASE_FOLDER + "../", 'package.json'), JSON.stringify(newPackage, null, 2));
