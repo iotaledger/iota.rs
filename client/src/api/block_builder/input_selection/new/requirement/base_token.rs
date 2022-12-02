4c646bc14cfc4c63fffdb66bc944dd77ee7b5532
Thibault Martinez <thibault@iota.org>
2022-12-02 15:44:41 +0100

    beep boop

--- client/src/api/block_builder/input_selection/new/requirement/base_token.rs
+++ client/src/api/block_builder/input_selection/new/requirement/base_token.rs
@@ -1,6 +1,8 @@
 // Copyright 2022 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+use std::collections::HashSet;
+
 use crate::{
     block::output::Output,
     error::{Error, Result},
@@ -14,6 +16,18 @@ pub(crate) fn base_token_sums(selected_inputs: &[InputSigningData], outputs: &[O
     (inputs_sum, outputs_sum)
 }
 
+// fn evaluate(inputs: impl Iterator<Item = InputSigningData>, diff: u64, newly_covered: &mut u64) {
+//     let current_selection = HashSet::new();
+
+//     while let Some(input) = inputs {
+//         newly_covered += input.output.amount();
+//         current_selection.insert(input.output_id());
+//         if diff <= newly_covered {
+//             break
+//         }
+//     }
+// }
+
 // TODO very dumb first draft.
 pub(crate) fn fulfill_base_token_requirement(
     available_inputs: &mut Vec<InputSigningData>,
@@ -21,14 +35,31 @@ pub(crate) fn fulfill_base_token_requirement(
     outputs: &[Output],
 ) -> Result<Vec<InputSigningData>> {
     let (inputs_sum, outputs_sum) = base_token_sums(selected_inputs, outputs);
+    let mut newly_selected_inputs = Vec::new();
 
-    if inputs_sum >= outputs_sum {
-        // Enough amount in the inputs to cover the outputs amount.
-        Ok(Vec::new())
-    } else {
+    if inputs_sum < outputs_sum {
         let diff = outputs_sum - inputs_sum;
         let mut newly_covered = 0;
-        let mut newly_selected_inputs = Vec::new();
+
+        // // 1. Basic with ED25519 address and nothing else
+        // // 2. Basic with ED25519 address and other things
+        // // 3. Moving funds of already transitioned other outputs
+        // // 4. Basic with other kind of address
+        // // 5. Other kinds of outputs
+
+        // // // TODO a first pass with even more basic restrictions?
+        // let basic_inputs = available_inputs.iter().filter(|input| input.output.is_basic());
+
+        // // evaluate(basic_inputs, diff, &mut newly_covered);
+
+        // let current_selection = HashSet::new();
+        // while let Some(input) = basic_inputs {
+        //     newly_covered += input.output.amount();
+        //     current_selection.insert(input.output_id());
+        //     if diff <= newly_covered {
+        //         break;
+        //     }
+        // }
 
         // TODO if consolidate strategy: sum all the lowest amount until diff is covered.
         available_inputs.sort_by(|left, right| left.output.amount().cmp(&right.output.amount()));
@@ -48,8 +79,8 @@ pub(crate) fn fulfill_base_token_requirement(
             });
         }
 
-        println!("{diff}");
-
-        Ok(newly_selected_inputs)
+        // println!("{diff}");
     }
+
+    Ok(newly_selected_inputs)
 }
