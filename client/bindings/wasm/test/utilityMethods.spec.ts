f436ccaa4c86d2547e065540d7ea00b24b71ed5e
Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
2022-12-05 15:19:11 +0100

    Add wasm binding workflow (#1438)
    * Add wasm binding workflow
    
    * Add test
    
    * Fix test
    
    * Bump cpufeatures
    
    * Add yarn build
    
    * Add target: wasm32-unknown-unknown
    
    * Format, fix test
    
    * Format
    
    * Update client/bindings/wasm/lib/bindings.ts
    
    * Bump minimist version

---
+++ client/bindings/wasm/test/utilityMethods.spec.ts
@@ -0,0 +1,50 @@
+import { Client } from '../node/lib';
+
+const offlineClient = new Client({});
+
+describe('Client utility methods', () => {
+    it('generates and validates mnemonic', async () => {
+        const mnemonic = await offlineClient.generateMnemonic();
+
+        // A mnemonic has 24 words
+        await expect(mnemonic.split(' ').length).toEqual(24);
+    });
+
+    it('converts address to hex and bech32', async () => {
+        const address =
+            'rms1qpllaj0pyveqfkwxmnngz2c488hfdtmfrj3wfkgxtk4gtyrax0jaxzt70zy';
+        const hexAddress = await offlineClient.bech32ToHex(address);
+
+        expect(hexAddress.slice(0, 2)).toBe('0x');
+
+        const bech32Address = await offlineClient.hexToBech32(
+            hexAddress,
+            'rms',
+        );
+
+        expect(bech32Address).toBe(address);
+    });
+
+    it('converts hex public key to bech32 address', async () => {
+        const hexPublicKey =
+            '0x2baaf3bca8ace9f862e60184bd3e79df25ff230f7eaaa4c7f03daa9833ba854a';
+
+        const address = await offlineClient.hexPublicKeyToBech32Address(
+            hexPublicKey,
+            'rms',
+        );
+
+        expect(address).toBe(
+            'rms1qzt0nhsf38nh6rs4p6zs5knqp6psgha9wsv74uajqgjmwc75ugupx4aaacx',
+        );
+    });
+
+    it('validates address', async () => {
+        const address =
+            'rms1qpllaj0pyveqfkwxmnngz2c488hfdtmfrj3wfkgxtk4gtyrax0jaxzt70zy';
+
+        const isAddressValid = await offlineClient.isAddressValid(address);
+
+        expect(isAddressValid).toBeTruthy();
+    });
+});
