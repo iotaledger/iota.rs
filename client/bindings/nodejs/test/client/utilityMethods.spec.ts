f4707662abf7ea49b729a45e27d39946a68a89d4
Thoralf Müller <thoralf.mue@gmail.com>
2022-12-06 11:38:02 +0100

    Format, rebuild before testing

--- client/bindings/nodejs/test/client/utilityMethods.spec.ts
+++ client/bindings/nodejs/test/client/utilityMethods.spec.ts
@@ -62,15 +62,22 @@ describe('Client utility methods', () => {
 
         const aliasId = await offlineClient.computeAliasId(outputId);
 
-        expect(aliasId).toBe("0xcf077d276686ba64c0404b9eb2d15556782113c5a1985f262b70f9964d3bbd7f");
+        expect(aliasId).toBe(
+            '0xcf077d276686ba64c0404b9eb2d15556782113c5a1985f262b70f9964d3bbd7f',
+        );
     });
 
     it('alias id to address', async () => {
         const aliasId =
             '0xcf077d276686ba64c0404b9eb2d15556782113c5a1985f262b70f9964d3bbd7f';
 
-        const aliasAddress = await offlineClient.aliasIdToBech32(aliasId, "rms");
+        const aliasAddress = await offlineClient.aliasIdToBech32(
+            aliasId,
+            'rms',
+        );
 
-        expect(aliasAddress).toBe("rms1pr8swlf8v6rt5exqgp9eavk324t8sggnckseshex9dc0n9jd8w7h7wcnhn7");
+        expect(aliasAddress).toBe(
+            'rms1pr8swlf8v6rt5exqgp9eavk324t8sggnckseshex9dc0n9jd8w7h7wcnhn7',
+        );
     });
 });
