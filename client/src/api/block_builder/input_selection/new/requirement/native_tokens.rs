36d7273fc8294e77be5d9f8d69604832858a200f
Thibault Martinez <thibault@iota.org>
2022-12-07 16:08:45 +0100

    Add get_minted_and_melted_native_tokens

--- client/src/api/block_builder/input_selection/new/requirement/native_tokens.rs
+++ client/src/api/block_builder/input_selection/new/requirement/native_tokens.rs
@@ -1,8 +1,74 @@
 // Copyright 2022 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-use super::{InputSelection, Requirement};
-use crate::{error::Result, secret::types::InputSigningData};
+use std::cmp::Ordering;
+
+use primitive_types::U256;
+
+use super::{InputSelection, OutputInfo, Requirement};
+use crate::{
+    block::output::{NativeToken, NativeTokensBuilder, Output, TokenScheme},
+    error::Result,
+    secret::types::InputSigningData,
+};
+
+fn get_minted_and_melted_native_tokens<'a>(
+    inputs: &[InputSigningData],
+    outputs: &[OutputInfo],
+) -> Result<(NativeTokensBuilder, NativeTokensBuilder)> {
+    let mut minted_native_tokens = NativeTokensBuilder::new();
+    let mut melted_native_tokens = NativeTokensBuilder::new();
+
+    for output in outputs {
+        if let Output::Foundry(output_foundry) = &output.output {
+            let TokenScheme::Simple(output_foundry_simple_ts) = output_foundry.token_scheme();
+            let mut initial_creation = true;
+
+            for input in inputs.clone() {
+                if let Output::Foundry(input_foundry) = &input.output {
+                    let token_id = output_foundry.token_id();
+                    if output_foundry.id() == input_foundry.id() {
+                        initial_creation = false;
+                        let TokenScheme::Simple(input_foundry_simple_ts) = input_foundry.token_scheme();
+                        match output_foundry_simple_ts
+                            .circulating_supply()
+                            .cmp(&input_foundry_simple_ts.circulating_supply())
+                        {
+                            Ordering::Greater => {
+                                let minted_native_token_amount = output_foundry_simple_ts.circulating_supply()
+                                    - input_foundry_simple_ts.circulating_supply();
+
+                                minted_native_tokens
+                                    .add_native_token(NativeToken::new(token_id, minted_native_token_amount)?)?;
+                            }
+                            Ordering::Less => {
+                                let melted_native_token_amount = input_foundry_simple_ts.circulating_supply()
+                                    - output_foundry_simple_ts.circulating_supply();
+
+                                melted_native_tokens
+                                    .add_native_token(NativeToken::new(token_id, melted_native_token_amount)?)?;
+                            }
+                            Ordering::Equal => {}
+                        }
+                    }
+                }
+            }
+
+            // If we created the foundry with this transaction, then we need to add the circulating supply as minted
+            // tokens
+            if initial_creation {
+                let circulating_supply = output_foundry_simple_ts.circulating_supply();
+
+                if circulating_supply != U256::from(0) {
+                    minted_native_tokens
+                        .add_native_token(NativeToken::new(output_foundry.token_id(), circulating_supply)?)?;
+                }
+            }
+        }
+    }
+
+    Ok((minted_native_tokens, melted_native_tokens))
+}
 
 impl InputSelection {
     pub(crate) fn fulfill_native_tokens_requirement(
@@ -14,7 +80,8 @@ impl InputSelection {
         // let input_native_tokens = gather_nts(selected_inputs);
         // let output_native_tokens = gather_nts(outputs);
 
-        // let (minted_native_tokens, melted_native_tokens) = get_minted_and_melted_nts(selected_inputs, outputs)?;
+        let (minted_native_tokens, melted_native_tokens) =
+            get_minted_and_melted_native_tokens(selected_inputs, &self.outputs)?;
 
         // let diffs = (input_native_tokens + minted) - (output + melted + burn);
 
