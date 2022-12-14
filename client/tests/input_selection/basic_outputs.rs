ee81dc29c0fdcc4bbdd8d561414ce49f084dbb8d
Thibault Martinez <thibault@iota.org>
2022-12-14 16:28:54 +0100

    Add basic sender tests

--- client/tests/input_selection/basic_outputs.rs
+++ client/tests/input_selection/basic_outputs.rs
@@ -1,15 +1,22 @@
 // Copyright 2022 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+use std::str::FromStr;
+
 use iota_client::{
     api::input_selection::new::{InputSelection, Requirement},
-    block::{address::Address, protocol::protocol_parameters},
+    block::{
+        address::Address,
+        output::{AliasId, NftId},
+        protocol::protocol_parameters,
+    },
     Error,
 };
 
 use crate::input_selection::{
-    build_basic_output, build_input_signing_data_most_basic_outputs, BECH32_ADDRESS, BECH32_ADDRESS_ALIAS_SENDER,
-    BECH32_ADDRESS_ED25519_SENDER, BECH32_ADDRESS_NFT_SENDER, BECH32_ADDRESS_REMAINDER,
+    build_basic_output, build_input_signing_data_alias_outputs, build_input_signing_data_most_basic_outputs,
+    build_input_signing_data_nft_outputs, ALIAS_ID_0, BECH32_ADDRESS, BECH32_ADDRESS_ALIAS_SENDER,
+    BECH32_ADDRESS_ED25519_SENDER, BECH32_ADDRESS_NFT_SENDER, BECH32_ADDRESS_REMAINDER, NFT_ID_0,
 };
 
 #[test]
@@ -162,6 +169,40 @@ fn not_enough_storage_deposit_for_remainder() {
 }
 
 #[test]
+fn ed25519_sender() {
+    let protocol_parameters = protocol_parameters();
+    let sender = Address::try_from_bech32(BECH32_ADDRESS_ED25519_SENDER).unwrap().1;
+
+    let inputs = build_input_signing_data_most_basic_outputs(vec![
+        (BECH32_ADDRESS, 2_000_000),
+        (BECH32_ADDRESS, 2_000_000),
+        (BECH32_ADDRESS_ED25519_SENDER, 1_000_000),
+        (BECH32_ADDRESS, 2_000_000),
+        (BECH32_ADDRESS, 2_000_000),
+    ]);
+    let outputs = vec![build_basic_output(
+        2_000_000,
+        BECH32_ADDRESS,
+        Some(BECH32_ADDRESS_ED25519_SENDER),
+    )];
+
+    let selected = InputSelection::build(outputs.clone(), inputs.clone(), protocol_parameters)
+        .finish()
+        .select()
+        .unwrap();
+
+    // Sender + another for amount
+    assert_eq!(selected.0.len(), 2);
+    assert!(selected
+        .0
+        .iter()
+        .find(|input| *input.output.as_basic().address() == sender)
+        .is_some());
+    // Provided output + remainder
+    assert_eq!(selected.1.len(), 2);
+}
+
+#[test]
 fn missing_ed25519_sender() {
     let protocol_parameters = protocol_parameters();
 
@@ -183,6 +224,44 @@ fn missing_ed25519_sender() {
 }
 
 #[test]
+fn alias_sender() {
+    let protocol_parameters = protocol_parameters();
+    let alias_id_0 = AliasId::from_str(ALIAS_ID_0).unwrap();
+
+    let mut inputs =
+        build_input_signing_data_most_basic_outputs(vec![(BECH32_ADDRESS, 2_000_000), (BECH32_ADDRESS, 2_000_000)]);
+    inputs.extend(build_input_signing_data_alias_outputs(vec![(
+        alias_id_0,
+        BECH32_ADDRESS,
+        1_000_000,
+    )]));
+    inputs.extend(build_input_signing_data_most_basic_outputs(vec![
+        (BECH32_ADDRESS, 2_000_000),
+        (BECH32_ADDRESS, 2_000_000),
+    ]));
+    let outputs = vec![build_basic_output(
+        2_000_000,
+        BECH32_ADDRESS,
+        Some(BECH32_ADDRESS_ALIAS_SENDER),
+    )];
+
+    let selected = InputSelection::build(outputs.clone(), inputs.clone(), protocol_parameters)
+        .finish()
+        .select()
+        .unwrap();
+
+    // Sender + another for amount
+    assert_eq!(selected.0.len(), 2);
+    assert!(selected
+        .0
+        .iter()
+        .find(|input| input.output.is_alias() && *input.output.as_alias().alias_id() == alias_id_0)
+        .is_some());
+    // Provided output + alias
+    assert_eq!(selected.1.len(), 2);
+}
+
+#[test]
 fn missing_alias_sender() {
     let protocol_parameters = protocol_parameters();
 
@@ -201,6 +280,44 @@ fn missing_alias_sender() {
         selected,
         Err(Error::UnfulfillableRequirement(Requirement::Sender(sender))) if sender.is_alias() && sender == Address::try_from_bech32(BECH32_ADDRESS_ALIAS_SENDER).unwrap().1
     ))
+}
+
+#[test]
+fn nft_sender() {
+    let protocol_parameters = protocol_parameters();
+    let nft_id_0 = NftId::from_str(NFT_ID_0).unwrap();
+
+    let mut inputs =
+        build_input_signing_data_most_basic_outputs(vec![(BECH32_ADDRESS, 2_000_000), (BECH32_ADDRESS, 2_000_000)]);
+    inputs.extend(build_input_signing_data_nft_outputs(vec![(
+        nft_id_0,
+        BECH32_ADDRESS,
+        1_000_000,
+    )]));
+    inputs.extend(build_input_signing_data_most_basic_outputs(vec![
+        (BECH32_ADDRESS, 2_000_000),
+        (BECH32_ADDRESS, 2_000_000),
+    ]));
+    let outputs = vec![build_basic_output(
+        2_000_000,
+        BECH32_ADDRESS,
+        Some(BECH32_ADDRESS_NFT_SENDER),
+    )];
+
+    let selected = InputSelection::build(outputs.clone(), inputs.clone(), protocol_parameters)
+        .finish()
+        .select()
+        .unwrap();
+
+    // Sender + another for amount
+    assert_eq!(selected.0.len(), 2);
+    assert!(selected
+        .0
+        .iter()
+        .find(|input| input.output.is_nft() && *input.output.as_nft().nft_id() == nft_id_0)
+        .is_some());
+    // Provided output + nft
+    assert_eq!(selected.1.len(), 2);
 }
 
 #[test]
--- client/tests/input_selection/mod.rs
+++ client/tests/input_selection/mod.rs
@@ -25,15 +25,15 @@ mod foundry_outputs;
 mod nft_outputs;
 
 const TOKEN_SUPPLY: u64 = 1_813_620_509_061_365;
+const ALIAS_ID_0: &str = "0x1111111111111111111111111111111111111111111111111111111111111111";
+const ALIAS_ID_1: &str = "0x2222222222222222222222222222222222222222222222222222222222222222";
+const NFT_ID_0: &str = "0x1111111111111111111111111111111111111111111111111111111111111111";
+const NFT_ID_1: &str = "0x2222222222222222222222222222222222222222222222222222222222222222";
 const BECH32_ADDRESS: &str = "rms1qr2xsmt3v3eyp2ja80wd2sq8xx0fslefmxguf7tshzezzr5qsctzc2f5dg6";
 const BECH32_ADDRESS_REMAINDER: &str = "rms1qrut5ajyfrtgjs325kd9chwfwyyy2z3fewy4vgy0vvdtf2pr8prg5u3zwjn";
 const BECH32_ADDRESS_ED25519_SENDER: &str = "rms1qqhvvur9xfj6yhgsxfa4f8xst7vz9zxeu3vcxds8mh4a6jlpteq9xrajhtf";
-const BECH32_ADDRESS_ALIAS_SENDER: &str = "rms1pr3xm8fm9ts5g96px0tpaytsqhrhgvqemprgyjlxc7gvrqyxg689kvxrn74";
-const BECH32_ADDRESS_NFT_SENDER: &str = "rms1zrsdjyctxcnkx9gh5jx3zkjh6uyutz3w76r2njj9wet95gp4d4gt5wdxrmz";
-const NFT_ID_0: &str = "0x0000000000000000000000000000000000000000000000000000000000000000";
-const NFT_ID_1: &str = "0x1111111111111111111111111111111111111111111111111111111111111111";
-const ALIAS_ID_0: &str = "0x0000000000000000000000000000000000000000000000000000000000000000";
-const ALIAS_ID_1: &str = "0x1111111111111111111111111111111111111111111111111111111111111111";
+const BECH32_ADDRESS_ALIAS_SENDER: &str = "rms1pqg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zws5524"; // Corresponds to ALIAS_ID_0
+const BECH32_ADDRESS_NFT_SENDER: &str = "rms1zqg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zyg3zxddmy7"; // Corresponds to NFT_ID_0
 
 fn build_basic_output(amount: u64, bech32_address: &str, bech32_sender: Option<&str>) -> Output {
     let mut builder = BasicOutputBuilder::new_with_amount(amount)
