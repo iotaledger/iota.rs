b5d9559a68b2ff30a6b47ad33248a2644413a093
Thibault Martinez <thibault@iota.org>
2023-01-12 14:55:53 +0100

    Remove some comments and prints

--- client/src/api/block_builder/input_selection/new/requirement/native_tokens.rs
+++ client/src/api/block_builder/input_selection/new/requirement/native_tokens.rs
@@ -115,7 +115,6 @@ pub(crate) fn get_native_tokens_diff(
 
 impl InputSelection {
     pub(crate) fn fulfill_native_tokens_requirement(&mut self) -> Result<(Vec<InputSigningData>, Option<Requirement>)> {
-        println!("NATIVE TOKENS FULFILLEMENT ---- ");
         let mut newly_selected_inputs = Vec::new();
         let mut newly_selected_ids = HashSet::new();
 
@@ -124,17 +123,9 @@ impl InputSelection {
         let (minted_native_tokens, melted_native_tokens) =
             get_minted_and_melted_native_tokens(&self.selected_inputs, &self.outputs)?;
 
-        println!("Input {input_native_tokens:?}");
-        println!("Output {output_native_tokens:?}");
-        println!("Minted {minted_native_tokens:?}");
-        println!("Melted {melted_native_tokens:?}");
-
         input_native_tokens.merge(minted_native_tokens)?;
         output_native_tokens.merge(melted_native_tokens)?;
         // TODO also merge burn
-
-        println!("Input merged {input_native_tokens:?}");
-        println!("Output merged {output_native_tokens:?}");
 
         // TODO weird that it happens in this direction?
         if let Some(diffs) = get_native_tokens_diff(&output_native_tokens, &input_native_tokens)? {
@@ -147,9 +138,8 @@ impl InputSelection {
                         .native_tokens()
                         .map_or(false, |native_tokens| native_tokens.contains(diff.token_id()))
                 });
-                println!("{diff:?}");
+
                 for input in inputs {
-                    println!("{input:?}");
                     amount += input
                         .output
                         .native_tokens()
@@ -177,43 +167,9 @@ impl InputSelection {
             }
         }
 
-        // let mut new_inputs = Vec::new();
-
-        // for (token_id, mut diff) in diffs {
-        //     // subtract the possible added native token amount from new selected inputs
-        //     let new_input_native_tokens = gather_nts(new_inputs);
-        //     let amount = new_input_native_tokens.get(token_id);
-        //     let diff = diff-amount;
-
-        //     if diff < 0 {
-        //         let native_token_inputs = available_inputs.filter(|input|input.has(token_id));
-        //         let inputs = native_token_inputs.smart_sum(diff);
-        //     }
-        // }
-
         self.available_inputs
             .retain(|input| !newly_selected_ids.contains(input.output_id()));
 
         Ok((newly_selected_inputs, None))
     }
 }
-
-// let input_native_tokens = gather_nts(selected_inputs);
-//     let output_native_tokens = gather_nts(outputs);
-//     let (minted_native_tokens, melted_native_tokens) = get_minted_and_melted_nts(selected_inputs, outputs)?;
-//     let native_tokens_diffs = (input_native_tokens + minted) - (output + melted + burn);
-
-//     // TODO: add required amount for storage deposit return unlock conditions of inputs to the require output
-// amount     let base_coin_diff = get_base_coin_diff(selected_inputs, output);
-
-//     // if !native_tokens_diffs.is_empty() || base_coin_diff != 0
-//     let remainder = OutputBuilder::new(base_coin_diff).with_native_tokens(native_tokens_diffs);
-
-//     let min_storage_deposit = remainder.min_storage_deposit();
-//     let base_coin_diff = base_coin_diff-min_storage_deposit;
-
-//     let mut new_inputs = Vec::new();
-//     if !native_tokens_diffs.is_empty() || base_coin_diff > 0{
-//     // TODO: select inputs for base coin diff until we have enough for the remainder if necesary
-//     }
-//     new_inputs
