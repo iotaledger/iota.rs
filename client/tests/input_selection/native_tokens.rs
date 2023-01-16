30ac64f39ae986af702a6fa143178992a22b5c8e
Thibault Martinez <thibault@iota.org>
2023-01-16 15:19:27 +0100

    Add burn_and_send_at_the_same_time test

--- client/tests/input_selection/native_tokens.rs
+++ client/tests/input_selection/native_tokens.rs
@@ -4,7 +4,7 @@
 use std::str::FromStr;
 
 use iota_client::{
-    api::input_selection::new::InputSelection,
+    api::input_selection::new::{Burn, InputSelection},
     block::{address::Address, output::TokenId, protocol::protocol_parameters},
     Error,
 };
@@ -258,4 +258,55 @@ fn insufficient_native_tokens_three_inputs() {
             found,
             required,
         }) if token_id == TokenId::from_str(TOKEN_ID_1).unwrap() && found == U256::from(300) && required == U256::from(301)));
+}
+
+#[test]
+fn burn_and_send_at_the_same_time() {
+    let protocol_parameters = protocol_parameters();
+
+    let inputs = build_inputs(vec![Basic(
+        2_000_000,
+        BECH32_ADDRESS,
+        Some(vec![(TOKEN_ID_1, 100), (TOKEN_ID_2, 100)]),
+        None,
+        None,
+    )]);
+    let outputs = build_outputs(vec![Basic(
+        1_000_000,
+        BECH32_ADDRESS,
+        Some(vec![(TOKEN_ID_1, 50), (TOKEN_ID_2, 100)]),
+        None,
+        None,
+    )]);
+
+    let selected = InputSelection::new(inputs.clone(), outputs.clone(), protocol_parameters)
+        .burn(Burn::new().add_native_token(TokenId::from_str(TOKEN_ID_1).unwrap(), U256::from(10)))
+        .select()
+        .unwrap();
+
+    assert!(unsorted_eq(&selected.inputs, &inputs));
+    assert_eq!(selected.outputs.len(), 2);
+    assert!(selected.outputs.contains(&outputs[0]));
+    selected.outputs.iter().for_each(|output| {
+        if !outputs.contains(output) {
+            assert!(output.is_basic());
+            assert_eq!(output.amount(), 1_000_000);
+            assert_eq!(output.as_basic().native_tokens().len(), 1);
+            assert_eq!(
+                output
+                    .as_basic()
+                    .native_tokens()
+                    .get(&TokenId::from_str(TOKEN_ID_1).unwrap())
+                    .unwrap()
+                    .amount(),
+                U256::from(40),
+            );
+            assert_eq!(output.as_basic().unlock_conditions().len(), 1);
+            assert_eq!(output.as_basic().features().len(), 0);
+            assert_eq!(
+                *output.as_basic().address(),
+                Address::try_from_bech32(BECH32_ADDRESS).unwrap().1
+            );
+        }
+    });
 }
