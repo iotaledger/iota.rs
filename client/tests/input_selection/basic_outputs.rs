ef8cd479c66ffbbbffabdb89ef83bb54f0746e00
Thibault Martinez <thibault@iota.org>
2023-01-17 14:16:10 +0100

    Some multiple basic outputs tests

--- client/tests/input_selection/basic_outputs.rs
+++ client/tests/input_selection/basic_outputs.rs
@@ -496,6 +496,29 @@ fn remainder_lower_than_rent() {
 }
 
 #[test]
+fn remainder_lower_than_rent_2() {
+    let protocol_parameters = protocol_parameters();
+
+    let inputs = build_inputs(vec![
+        Basic(1_000_000, BECH32_ADDRESS_ED25519_0, None, None, None),
+        Basic(2_000_000, BECH32_ADDRESS_ED25519_0, None, None, None),
+    ]);
+    let outputs = build_outputs(vec![Basic(2_800_000, BECH32_ADDRESS_ED25519_0, None, None, None)]);
+
+    let selected = InputSelection::new(inputs, outputs, protocol_parameters).select();
+
+    assert!(matches!(
+        selected,
+        Err(Error::BlockError(
+            iota_types::block::Error::InsufficientStorageDepositAmount {
+                amount: 200_000,
+                required: 213_000,
+            }
+        ))
+    ));
+}
+
+#[test]
 fn one_provided_one_needed() {
     let protocol_parameters = protocol_parameters();
 
@@ -526,4 +549,69 @@ fn insufficient_amount() {
             required: 1_250_000,
         })
     ));
+}
+
+#[test]
+fn two_inputs_remainder_2() {
+    let protocol_parameters = protocol_parameters();
+
+    let inputs = build_inputs(vec![
+        Basic(1_000_000, BECH32_ADDRESS_ED25519_0, None, None, None),
+        Basic(2_000_000, BECH32_ADDRESS_ED25519_0, None, None, None),
+    ]);
+    let outputs = build_outputs(vec![Basic(500_000, BECH32_ADDRESS_ED25519_0, None, None, None)]);
+
+    let selected = InputSelection::new(inputs.clone(), outputs.clone(), protocol_parameters)
+        .select()
+        .unwrap();
+
+    assert_eq!(selected.inputs.len(), 1);
+    assert!(selected.inputs.contains(&inputs[0]));
+    assert_eq!(selected.outputs.len(), 2);
+    assert!(selected.outputs.contains(&outputs[0]));
+    selected.outputs.iter().for_each(|output| {
+        if !outputs.contains(output) {
+            assert!(output.is_basic());
+            assert_eq!(output.amount(), 500_000);
+            assert_eq!(output.as_basic().native_tokens().len(), 0);
+            assert_eq!(output.as_basic().unlock_conditions().len(), 1);
+            assert_eq!(output.as_basic().features().len(), 0);
+            assert_eq!(
+                *output.as_basic().address(),
+                Address::try_from_bech32(BECH32_ADDRESS_ED25519_0).unwrap().1
+            );
+        }
+    });
+}
+
+#[test]
+fn two_inputs_remainder_3() {
+    let protocol_parameters = protocol_parameters();
+
+    let inputs = build_inputs(vec![
+        Basic(1_000_000, BECH32_ADDRESS_ED25519_0, None, None, None),
+        Basic(2_000_000, BECH32_ADDRESS_ED25519_0, None, None, None),
+    ]);
+    let outputs = build_outputs(vec![Basic(1_750_000, BECH32_ADDRESS_ED25519_0, None, None, None)]);
+
+    let selected = InputSelection::new(inputs.clone(), outputs.clone(), protocol_parameters)
+        .select()
+        .unwrap();
+
+    assert!(unsorted_eq(&selected.inputs, &inputs));
+    assert_eq!(selected.outputs.len(), 2);
+    assert!(selected.outputs.contains(&outputs[0]));
+    selected.outputs.iter().for_each(|output| {
+        if !outputs.contains(output) {
+            assert!(output.is_basic());
+            assert_eq!(output.amount(), 1_250_000);
+            assert_eq!(output.as_basic().native_tokens().len(), 0);
+            assert_eq!(output.as_basic().unlock_conditions().len(), 1);
+            assert_eq!(output.as_basic().features().len(), 0);
+            assert_eq!(
+                *output.as_basic().address(),
+                Address::try_from_bech32(BECH32_ADDRESS_ED25519_0).unwrap().1
+            );
+        }
+    });
 }
