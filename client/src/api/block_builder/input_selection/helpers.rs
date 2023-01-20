8c23c9834c895a7767839b5bc28317f5ab348b0a
Thibault Martinez <thibault@iota.org>
2023-01-20 12:58:38 +0100

    Bring back minimum_storage_deposit_basic_output

--- client/src/api/block_builder/input_selection/helpers.rs
+++ client/src/api/block_builder/input_selection/helpers.rs
@@ -5,10 +5,13 @@
 
 use iota_types::block::{
     address::{Address, AliasAddress, Ed25519Address, NftAddress},
-    output::Output,
+    output::{
+        unlock_condition::{AddressUnlockCondition, UnlockCondition},
+        BasicOutputBuilder, NativeTokens, Output, Rent, RentStructure,
+    },
 };
 
-use crate::secret::types::InputSigningData;
+use crate::{secret::types::InputSigningData, Result};
 
 // Dedup inputs by output id, because other data could be different, even if it's the same output
 // TODO remove ?
@@ -101,4 +104,24 @@ pub(crate) fn _sort_input_signing_data(inputs: Vec<InputSigningData>) -> crate::
     }
 
     Ok(sorted_inputs)
+}
+
+/// Computes the minimum storage deposit amount that a basic output needs to have with an [AddressUnlockCondition] and
+/// optional [NativeTokens].
+pub fn minimum_storage_deposit_basic_output(
+    config: &RentStructure,
+    address: &Address,
+    native_tokens: &Option<NativeTokens>,
+    token_supply: u64,
+) -> Result<u64> {
+    let address_condition = UnlockCondition::Address(AddressUnlockCondition::new(*address));
+    let mut basic_output_builder = BasicOutputBuilder::new_with_amount(Output::AMOUNT_MIN)?;
+    if let Some(native_tokens) = native_tokens {
+        basic_output_builder = basic_output_builder.with_native_tokens(native_tokens.clone());
+    }
+    let basic_output = basic_output_builder
+        .add_unlock_condition(address_condition)
+        .finish_output(token_supply)?;
+
+    Ok(basic_output.rent_cost(config))
 }
