b6a307ad8dd0e5c35cafec4549fdf7f5f879f01e
Thibault Martinez <thibault@iota.org>
2023-01-30 10:05:18 +0100

    Use default instead of new

--- pow/tests/score.rs
+++ pow/tests/score.rs
@@ -1,15 +1,15 @@
 // Copyright 2020-2021 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-use iota_pow::score::PowScorer;
-
 // Tests are from:
 // https://github.com/iotaledger/tips/blob/main/tips/TIP-0012/tip-0012.md#example
 // https://github.com/Wollac/iota-crypto-demo/blob/master/pkg/pow/pow_test.go#L26
 
+use iota_pow::score::PowScorer;
+
 #[test]
 fn pow_score() {
-    let mut pow = PowScorer::new();
+    let mut pow = PowScorer::default();
 
     let block: [u8; 21] = [
         0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x2c, 0x20, 0x57, 0x6f, 0x72, 0x6c, 0x64, 0x21, 0x5e, 0xe6, 0xaa, 0xaa, 0xaa,
