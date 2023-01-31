8b2edbcbe4259acd5a9eb3d258e8be97dbb90096
Thoralf Müller <thoralf.mue@gmail.com>
2023-01-31 11:34:22 +0100

    New url

--- bindings/python/examples/composite_example.py
+++ bindings/python/examples/composite_example.py
@@ -265,7 +265,7 @@ def peer():
 
 def quorum():
     node_1 = 'https://api.lb-0.h.chrysalis-devnet.iota.cafe'
-    node_2 = 'https://api.thin-hornet-1.h.chrysalis-devnet.iota.cafe'
+    node_2 = 'https://api.lb-1.h.chrysalis-devnet.iota.cafe'
     node_3 = 'https://api.lb-0.h.chrysalis-devnet.iota.cafe'
     try:
         client = iota_client.Client(nodes_name_password=[
