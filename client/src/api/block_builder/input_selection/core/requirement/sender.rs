02bda698a329c4ff19c4a4a1246468957fe318cd
Thibault Martinez <thibault@iota.org>
2023-02-08 11:44:31 +0100

    sender/issuer logs

--- client/src/api/block_builder/input_selection/core/requirement/sender.rs
+++ client/src/api/block_builder/input_selection/core/requirement/sender.rs
@@ -63,11 +63,15 @@ impl InputSelection {
         address: Address,
     ) -> Result<(Vec<(InputSigningData, Option<AliasTransition>)>, Option<Requirement>)> {
         // Checks if the requirement is already fulfilled.
-        if self
+        if let Some(output) = self
             .selected_inputs
             .iter()
-            .any(|input| selected_has_ed25519_address(input, self.outputs.as_slice(), &address, self.timestamp))
+            .find(|input| selected_has_ed25519_address(input, self.outputs.as_slice(), &address, self.timestamp))
         {
+            log::debug!(
+                "{address:?} sender requirement already fulfilled by {:?}",
+                output.output_id()
+            );
             return Ok((Vec::new(), None));
         }
 
@@ -87,7 +91,11 @@ impl InputSelection {
             self.available_inputs.iter().enumerate().find_map(|(index, input)| {
                 if !input.output.is_basic() {
                     let (found, alias_transition) = available_has_ed25519_address(input, &address, self.timestamp);
-                    if found { Some((index, alias_transition)) } else { None }
+                    if found {
+                        Some((index, alias_transition))
+                    } else {
+                        None
+                    }
                 } else {
                     None
                 }
@@ -96,7 +104,16 @@ impl InputSelection {
 
         match found {
             Some((index, alias_transition)) => {
-                Ok((vec![(self.available_inputs.swap_remove(index), alias_transition)], None))
+                // Remove the output from the available inputs, swap to make it O(1).
+                let input = self.available_inputs.swap_remove(index);
+
+                log::debug!(
+                    "{address:?} sender requirement fulfilled by {:?} (alias transition {:?})",
+                    input.output_id(),
+                    alias_transition
+                );
+
+                Ok((vec![(input, alias_transition)], None))
             }
             None => Err(Error::UnfulfillableRequirement(Requirement::Sender(address))),
         }
@@ -110,6 +127,8 @@ impl InputSelection {
         match address {
             Address::Ed25519(_) => self.fulfill_ed25519_address_requirement(address),
             Address::Alias(alias_address) => {
+                log::debug!("Treating {address:?} sender requirement as an alias requirement");
+
                 match self.fulfill_alias_requirement(alias_address.into_alias_id(), AliasTransition::State) {
                     Ok(res) => Ok(res),
                     Err(Error::UnfulfillableRequirement(Requirement::Alias(_, _))) => {
@@ -118,13 +137,17 @@ impl InputSelection {
                     Err(e) => Err(e),
                 }
             }
-            Address::Nft(nft_address) => match self.fulfill_nft_requirement(nft_address.into_nft_id()) {
-                Ok(res) => Ok(res),
-                Err(Error::UnfulfillableRequirement(Requirement::Nft(_))) => {
-                    Err(Error::UnfulfillableRequirement(Requirement::Sender(address)))
+            Address::Nft(nft_address) => {
+                log::debug!("Treating {address:?} sender requirement as an nft requirement");
+
+                match self.fulfill_nft_requirement(nft_address.into_nft_id()) {
+                    Ok(res) => Ok(res),
+                    Err(Error::UnfulfillableRequirement(Requirement::Nft(_))) => {
+                        Err(Error::UnfulfillableRequirement(Requirement::Sender(address)))
+                    }
+                    Err(e) => Err(e),
                 }
-                Err(e) => Err(e),
-            },
+            }
         }
     }
 }
