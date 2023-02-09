1122606bc3916a27e8daa8bbcf8b862224e75716
Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
2023-02-09 09:38:33 +0100

    Bump versions (#1566)

--- client/bindings/python/setup.py
+++ client/bindings/python/setup.py
@@ -22,10 +22,10 @@ def get_py_version_cfgs():
 
 setup(
     name="iota_client",
-    version="1.0.0.rc.1",
+    version="1.0.0.rc.2",
     classifiers=[
         "License :: SPDX-License-Identifier ::  Apache-2.0",
-        "Development Status :: 1.0.0.rc.1",
+        "Development Status :: 1.0.0.rc.2",
         "Intended Audience :: Developers",
         "Programming Language :: Python",
         "Programming Language :: Rust",
