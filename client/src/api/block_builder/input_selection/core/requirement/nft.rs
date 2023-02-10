367b606a21860136f62116ff01c9a9bf04a95f96
Thibault Martinez <thibault@iota.org>
2023-02-10 11:19:37 +0100

    NFT nits

--- client/src/api/block_builder/input_selection/core/requirement/nft.rs
+++ client/src/api/block_builder/input_selection/core/requirement/nft.rs
@@ -8,20 +8,23 @@ use crate::{
     secret::types::InputSigningData,
 };
 
-/// Checks if an output is an nft with a given non null nft ID.
-/// Calling it with a null nft ID may lead to undefined behavior.
-pub(crate) fn is_nft_with_id_non_null(output: &Output, nft_id: &NftId) -> bool {
+/// Checks if an output is an nft with a given nft ID.
+/// Assumes that the output nft ID can be null and hashes the output ID.
+/// Use when not sure if the nft has been assigned a proper ID already.
+pub(crate) fn is_nft_with_id(output: &Output, nft_id: &NftId, output_id: &OutputId) -> bool {
     if let Output::Nft(nft) = output {
-        nft.nft_id() == nft_id
+        &nft.nft_id_non_null(output_id) == nft_id
     } else {
         false
     }
 }
 
-/// Checks if an output is an nft with output ID that matches the given nft ID.
-pub(crate) fn is_nft_with_id(output: &Output, output_id: &OutputId, nft_id: &NftId) -> bool {
+/// Checks if an output is an nft with a given nft ID.
+/// Assumes that the output nft ID is non null to avoid an output ID hash.
+/// Only use when sure that the nft has been assigned a proper ID already.
+pub(crate) fn is_nft_with_id_non_null(output: &Output, nft_id: &NftId) -> bool {
     if let Output::Nft(nft) = output {
-        &nft.nft_id_non_null(output_id) == nft_id
+        nft.nft_id() == nft_id
     } else {
         false
     }
@@ -34,22 +37,22 @@ impl InputSelection {
         nft_id: NftId,
     ) -> Result<Vec<(InputSigningData, Option<AliasTransition>)>> {
         // Check if the requirement is already fulfilled.
-        if let Some(output) = self
+        if let Some(input) = self
             .selected_inputs
             .iter()
-            .find(|input| is_nft_with_id(&input.output, input.output_id(), &nft_id))
+            .find(|input| is_nft_with_id(&input.output, &nft_id, input.output_id()))
         {
-            log::debug!("{nft_id:?} requirement already fulfilled by {:?}", output.output_id());
-            return Ok(Vec::new());
+            log::debug!("{nft_id:?} requirement already fulfilled by {:?}", input.output_id());
+            return Ok(vec![]);
         }
 
         // Check if the requirement can be fulfilled.
         let index = self
             .available_inputs
             .iter()
-            .position(|input| is_nft_with_id(&input.output, input.output_id(), &nft_id))
+            .position(|input| is_nft_with_id(&input.output, &nft_id, input.output_id()))
             .ok_or(Error::UnfulfillableRequirement(Requirement::Nft(nft_id)))?;
-        // Remove the output from the available inputs, swap to make it O(1).
+        // Remove the input from the available inputs, swap to make it O(1).
         let input = self.available_inputs.swap_remove(index);
 
         log::debug!("{nft_id:?} requirement fulfilled by {:?}", input.output_id());
