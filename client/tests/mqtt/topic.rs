2714af7fcea37a35357dc20f401762aa3e7bbcb9
Thibault Martinez <thibault@iota.org>
2023-02-20 18:47:17 +0100

    Fmt

--- client/tests/mqtt/topic.rs
+++ client/tests/mqtt/topic.rs
@@ -18,25 +18,29 @@ fn valid_topics() {
         Topic::try_new("block-metadata/0x36845227a59864ac12d3d2389fcb4ea0bdd1a5d1d4ed464bde3154216c3246c4").is_ok()
     );
     assert!(Topic::try_new("block-metadata/referenced").is_ok());
-    assert!(Topic::try_new(
-        "transactions/0x36845227a59864ac12d3d2389fcb4ea0bdd1a5d1d4ed464bde3154216c3246c4/included-block"
-    )
-    .is_ok());
+    assert!(
+        Topic::try_new(
+            "transactions/0x36845227a59864ac12d3d2389fcb4ea0bdd1a5d1d4ed464bde3154216c3246c4/included-block"
+        )
+        .is_ok()
+    );
     assert!(Topic::try_new("outputs/0x36845227a59864ac12d3d2389fcb4ea0bdd1a5d1d4ed464bde3154216c3246c40000").is_ok());
     assert!(Topic::try_new("outputs/alias/0xb21517992e96865d5fd90b403fe05fe25c6d4acfb6cdd6e7c9bbfb4266d05151").is_ok());
     assert!(Topic::try_new("outputs/nft/0x38500750eb788bfb89b4589634a82b0cee9c6a9724bafde505ffa1bb875ab0b5").is_ok());
-    assert!(Topic::try_new(
-        "outputs/foundry/0x08e10a5c7bcfdce48ff500156040f7548ca511d79a6e253a22759116c2ae8c818d0100000000"
-    )
-    .is_ok());
+    assert!(
+        Topic::try_new(
+            "outputs/foundry/0x08e10a5c7bcfdce48ff500156040f7548ca511d79a6e253a22759116c2ae8c818d0100000000"
+        )
+        .is_ok()
+    );
     assert!(
         Topic::try_new("outputs/unlock/address/iota1qrwfnskm4f7utdrxqnkfntfqxehtpj8s0kf68zkcwm0yrhuemzjp5sjfw5v")
             .is_ok()
     );
-    assert!(Topic::try_new(
-        "outputs/unlock/address/iota1qrwfnskm4f7utdrxqnkfntfqxehtpj8s0kf68zkcwm0yrhuemzjp5sjfw5v/spent"
-    )
-    .is_ok());
+    assert!(
+        Topic::try_new("outputs/unlock/address/iota1qrwfnskm4f7utdrxqnkfntfqxehtpj8s0kf68zkcwm0yrhuemzjp5sjfw5v/spent")
+            .is_ok()
+    );
     assert!(Topic::try_new("receipts").is_ok());
 }
 
@@ -68,7 +72,9 @@ fn invalid_tags() {
         Err(Error::InvalidMqttTopic(_))
     ));
     assert!(matches!(
-        Topic::try_new("blocks/tagged-data/0xb21517992e96865d5fd90b403fe05fe25c6d4acfb6cdd6e7c9bbfb4266d05151b21517992e96865d5fd90b403fe05fe25c6d4acfb6cdd6e7c9bbfb4266d05151ff"),
+        Topic::try_new(
+            "blocks/tagged-data/0xb21517992e96865d5fd90b403fe05fe25c6d4acfb6cdd6e7c9bbfb4266d05151b21517992e96865d5fd90b403fe05fe25c6d4acfb6cdd6e7c9bbfb4266d05151ff"
+        ),
         Err(Error::InvalidMqttTopic(_))
     ));
     // Invalid chars.
