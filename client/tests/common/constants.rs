e90c4f34a934e440dd4b6f438a9d9ef7653f222b
Thoralf Müller <thoralf.mue@gmail.com>
2023-03-03 12:21:30 +0100

    Add test_send_basic_output

---
+++ client/tests/common/constants.rs
@@ -0,0 +1,6 @@
+// Copyright 2023 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+pub static NODE_LOCAL: &str = "http://localhost:14265";
+
+pub static FAUCET_URL: &str = "http://localhost:8091/api/enqueue";
