931e9e21030e0ab1c3ff147d8139923d46774f55
Dr-Electron <dr-electr0n@protonmail.com>
2023-03-09 22:49:47 +0100

    Unify "Build a Basic Output" examples

--- client/bindings/nodejs/examples/11_build_output.ts
+++ client/bindings/nodejs/examples/11_build_output.ts
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { Client, initLogger } from '@iota/client';
+import type { UnlockConditionTypes } from '@iota/types';
 require('dotenv').config({ path: '../.env' });
 
 // Run with command:
@@ -19,40 +20,92 @@ async function run() {
     });
 
     try {
-        if (!process.env.NON_SECURE_USE_OF_DEVELOPMENT_MNEMONIC_1) {
-            throw new Error('.env mnemonic is undefined, see .env.example');
-        }
-        const secretManager = {
-            mnemonic: process.env.NON_SECURE_USE_OF_DEVELOPMENT_MNEMONIC_1,
+        const hexAddress = await client.bech32ToHex(
+            'rms1qpllaj0pyveqfkwxmnngz2c488hfdtmfrj3wfkgxtk4gtyrax0jaxzt70zy',
+        );
+
+        const addressUnlockCondition: UnlockConditionTypes = {
+            type: 0,
+            address: {
+                type: 0,
+                pubKeyHash: hexAddress,
+            },
         };
 
-        const addresses = await client.generateAddresses(secretManager, {
-            range: {
-                start: 0,
-                end: 1,
-            },
-        });
-
-        const hexAddress = await client.bech32ToHex(addresses[0]);
-
-        // most simple basic output
+        // Build most basic output with amound and a single address unlock condition
         const basicOutput = await client.buildBasicOutput({
             amount: '1000000',
-            unlockConditions: [
+            unlockConditions: [addressUnlockCondition],
+        });
+
+        console.log(JSON.stringify(basicOutput, null, 2));
+
+        // Output with metadata feature block
+        const basicOutputWithMetadata = await client.buildBasicOutput({
+            amount: '1000000',
+            unlockConditions: [addressUnlockCondition],
+            features: [
                 {
-                    type: 0,
-                    address: {
-                        type: 0,
-                        pubKeyHash: hexAddress,
-                    },
+                    type: 2,
+                    data: '0x0d25',
                 },
             ],
         });
 
-        console.log(basicOutput);
+        console.log(JSON.stringify(basicOutputWithMetadata, null, 2));
+
+        // Output with storage deposit return
+        const basicOutputWithStorageReturn = await client.buildBasicOutput({
+            amount: '1000000',
+            unlockConditions: [
+                addressUnlockCondition,
+                {
+                    type: 1,
+                    returnAddress: {
+                        type: 0,
+                        pubKeyHash: hexAddress,
+                    },
+                    amount: '1000000',
+                },
+            ],
+        });
+
+        console.log(JSON.stringify(basicOutputWithStorageReturn, null, 2));
+
+        // Output with expiration
+        const basicOutputWithExpiration = await client.buildBasicOutput({
+            amount: '1000000',
+            unlockConditions: [
+                addressUnlockCondition,
+                {
+                    type: 3,
+                    returnAddress: {
+                        type: 0,
+                        pubKeyHash: hexAddress,
+                    },
+                    unixTime: 1,
+                },
+            ],
+        });
+
+        console.log(JSON.stringify(basicOutputWithExpiration, null, 2));
+
+        // Output with timelock
+        const basicOutputWithTimelock = await client.buildBasicOutput({
+            amount: '1000000',
+            unlockConditions: [
+                addressUnlockCondition,
+                {
+                    type: 2,
+                    unixTime: 1,
+                },
+            ],
+        });
+
+        console.log(JSON.stringify(basicOutputWithTimelock, null, 2));
     } catch (error) {
         console.error('Error: ', error);
     }
 }
 
-run();
+run().then(() => process.exit());
--- client/examples/output/basic.rs
+++ client/examples/output/basic.rs
@@ -4,16 +4,17 @@
 //! cargo run --example basic --release
 
 use iota_client::{
-    block::output::{
-        feature::MetadataFeature,
-        unlock_condition::{
-            AddressUnlockCondition, ExpirationUnlockCondition, StorageDepositReturnUnlockCondition,
-            TimelockUnlockCondition, UnlockCondition,
+    block::{
+        address::Address,
+        output::{
+            feature::MetadataFeature,
+            unlock_condition::{
+                AddressUnlockCondition, ExpirationUnlockCondition, StorageDepositReturnUnlockCondition,
+                TimelockUnlockCondition, UnlockCondition,
+            },
+            BasicOutputBuilder, Feature,
         },
-        BasicOutputBuilder, Feature,
     },
-    secret::SecretManager,
-    utils::request_funds_from_faucet,
     Client, Result,
 };
 
@@ -22,26 +23,16 @@ use iota_client::{
 #[tokio::main]
 async fn main() -> Result<()> {
     // This example uses dotenv, which is not safe for use in production!
-    // Configure your own mnemonic in the ".env" file. Since the output amount cannot be zero, the seed must contain
-    // non-zero balance.
     dotenv::dotenv().ok();
 
     let node_url = std::env::var("NODE_URL").unwrap();
-    let faucet_url = std::env::var("FAUCET_URL").unwrap();
 
     // Create a client instance.
     let client = Client::builder().with_node(&node_url)?.finish()?;
 
-    let secret_manager =
-        SecretManager::try_from_mnemonic(&std::env::var("NON_SECURE_USE_OF_DEVELOPMENT_MNEMONIC_1").unwrap())?;
-
     let token_supply = client.get_token_supply().await?;
 
-    let address = client.get_addresses(&secret_manager).with_range(0..1).get_raw().await?[0];
-    println!(
-        "{}",
-        request_funds_from_faucet(&faucet_url, &address.to_bech32(client.get_bech32_hrp().await?)).await?
-    );
+    let address = Address::try_from_bech32("rms1qpllaj0pyveqfkwxmnngz2c488hfdtmfrj3wfkgxtk4gtyrax0jaxzt70zy")?.1;
 
     let basic_output_builder = BasicOutputBuilder::new_with_amount(1_000_000)?
         .add_unlock_condition(UnlockCondition::Address(AddressUnlockCondition::new(address)));
@@ -57,9 +48,8 @@ async fn main() -> Result<()> {
         // with storage deposit return
         basic_output_builder
             .clone()
-            .with_amount(234_100)?
             .add_unlock_condition(UnlockCondition::StorageDepositReturn(
-                StorageDepositReturnUnlockCondition::new(address, 234_000, token_supply)?,
+                StorageDepositReturnUnlockCondition::new(address, 1000000, token_supply)?,
             ))
             .finish_output(token_supply)?,
         // with expiration
@@ -74,15 +64,7 @@ async fn main() -> Result<()> {
             .finish_output(token_supply)?,
     ];
 
-    let block = client
-        .block()
-        .with_secret_manager(&secret_manager)
-        .with_outputs(outputs)?
-        .finish()
-        .await?;
+    println!("{outputs:#?}");
 
-    println!("Transaction sent: {node_url}/api/core/v2/blocks/{}", block.id());
-    println!("Block metadata: {node_url}/api/core/v2/blocks/{}/metadata", block.id());
-    let _ = client.retry_until_included(&block.id(), None, None).await?;
     Ok(())
 }
