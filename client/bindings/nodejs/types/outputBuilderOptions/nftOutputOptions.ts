3e1bfc1673f20655daf1afe3632c36321e587b6d
Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
2023-03-09 16:59:52 +0100

    Unify alias output examples (#1643)
    * Unify alias output examples, change stateMetadata to String, fix Python error, fix Java binding path
    
    * Fmt
    
    * Add HexEncodedString type
    
    * Address review comments

--- client/bindings/nodejs/types/outputBuilderOptions/nftOutputOptions.ts
+++ client/bindings/nodejs/types/outputBuilderOptions/nftOutputOptions.ts
@@ -1,10 +1,11 @@
 import type { FeatureTypes } from '@iota/types';
+import type { HexEncodedString } from '../../lib';
 import type { IBasicOutputBuilderOptions } from './basicOutputOptions';
 
 /**
  * Options for building an Nft Output
  */
 export interface INftOutputBuilderOptions extends IBasicOutputBuilderOptions {
-    nftId: string;
+    nftId: HexEncodedString;
     immutableFeatures?: FeatureTypes[];
 }
