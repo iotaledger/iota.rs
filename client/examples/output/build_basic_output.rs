edd0e8e0e2e0252f7bbb1a9f15c8836383c99736
Dr-Electron <dr-electr0n@protonmail.com>
2023-03-13 14:30:26 +0100

    I need to learn how to fmt

--- client/examples/output/build_basic_output.rs
+++ client/examples/output/build_basic_output.rs
@@ -43,7 +43,9 @@ async fn main() -> Result<()> {
         // with metadata feature block
         basic_output_builder
             .clone()
-            .add_feature(Feature::Metadata(MetadataFeature::new("Hello, World!".as_bytes().to_owned())?))
+            .add_feature(Feature::Metadata(MetadataFeature::new(
+                "Hello, World!".as_bytes().to_owned(),
+            )?))
             .finish_output(token_supply)?,
         // with storage deposit return
         basic_output_builder
